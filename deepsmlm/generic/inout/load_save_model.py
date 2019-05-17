c10f0ccb975be51a7c6ebeec2a69edbb7165199c
Lucas-Rapahel Müller <gitdev@LRM.photo>
2019-05-17 16:40:01 +0200

    Incorporate new suffix based on timestamp in LoadSave. Trigger new save by metric. Add test.

--- deepsmlm/generic/inout/load_save_model.py
+++ deepsmlm/generic/inout/load_save_model.py
@@ -1,12 +1,28 @@
+import math
+import time
 import torch
 
 
 class LoadSaveModel:
-    def __init__(self, model_instance, output_file, input_file=None):
+    def __init__(self, model_instance, output_file, input_file=None, name_time_interval=(60*60), better_th=1e-3):
+        """
+
+        :param model_instance:
+        :param output_file:
+        :param input_file:
+        :param name_time_interval:
+        :param better_th: relative threshold on a metric to determine whether a model is better or not.
+        """
         self.warmstart_file = input_file
         self.output_file = output_file
-        self.output_file_suffix = 0
+        self.output_file_suffix = -1  # because will be increased to one in the first round
         self.model = model_instance
+        self.name_time_interval = name_time_interval
+
+        self._last_saved = 0  # timestamp when it was saved last
+        self._new_name_time = 0   # timestamp when the name changed last
+        self._best_metric_val = math.inf
+        self.better_th = better_th
 
     def load_init(self, cuda=torch.cuda.is_available()):
         model = self.model
@@ -25,12 +41,28 @@ class LoadSaveModel:
         model.eval()
         return model
 
-    def save(self, model, trigger_new_name=False):
-        if trigger_new_name:
+    def save(self, model, metric_val=None):
+        """
+        Saves the model if it is better than before
+        :param model:
+        :param metric_val:
+        :return:
+        """
+        """If relative difference to previous value is less than threshold difference, do not save."""
+        rel_diff = metric_val / self._best_metric_val
+        if rel_diff <= 1 - self.better_th:
+            self._best_metric_val = metric_val
+        else:
+            return
+
+        """After a certain period, change the suffix."""
+        if (time.time() > self._new_name_time + self.name_time_interval) or metric_val is None:
             self.output_file_suffix += 1
 
-        # a bit hacky
+        """Determine file name and save."""
         fname = self.output_file[:-3] + '_' + str(self.output_file_suffix) + '.pt'
         torch.save(model.state_dict(), fname)
         print('Saved model to file: {}'.format(fname))
+
+        self._last_saved = time.time()
 
---
+++ deepsmlm/test/test_loadSaveModel.py
@@ -0,0 +1,43 @@
+import os
+import time
+import glob
+
+import pytest
+import deepsmlm.neuralfitter.models.model as model
+import deepsmlm.generic.inout.load_save_model as io_model
+
+deepsmlm_root = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 os.pardir, os.pardir)) + '/'
+
+
+@pytest.fixture
+def unet():
+    """Inits an arbitrary UNet."""
+    return model.UNet(3, 5)
+
+
+@pytest.fixture
+def model_interface(unet):
+    """Inits the i/o interface."""
+    return io_model.LoadSaveModel(unet, deepsmlm_root + 'deepsmlm/test/assets/test_load_save.pt', None, 1, 1e-3)
+
+
+def test_save(model_interface, unet):
+    """Tests saving the model, and tests whether a new suffix is appended."""
+    model_interface.save(unet, None)
+    exists = os.path.isfile(deepsmlm_root + 'deepsmlm/test/assets/test_load_save_0.pt')
+    if not exists:
+        pytest.fail("Model could not be found after saving.")
+
+    time.sleep(2)
+    model_interface.save(unet, None)
+    exists = os.path.isfile(deepsmlm_root + 'deepsmlm/test/assets/test_load_save_1.pt')
+    if not exists:
+        pytest.fail("Model could not be found after saving.")
+
+
+def test_load_init(model_interface):
+    model_interface.load_init(deepsmlm_root + 'deepsmlm/test/assets/test_load_save_0.pt')
+
+
