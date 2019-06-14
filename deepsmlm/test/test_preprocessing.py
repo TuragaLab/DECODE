bdf967b5e773f7c66aa17872618adb7548c95143
Lucas-Rapahel Müller <gitdev@LRM.photo>
2019-06-14 15:28:10 +0200

    Change test to pytest and add check whether values are between specific range.

--- deepsmlm/test/test_preprocessing.py
+++ deepsmlm/test/test_preprocessing.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import torch
+import pytest
 
 from deepsmlm.generic.emitter import EmitterSet
 from deepsmlm.generic.psf_kernel import DeltaPSF
@@ -37,29 +38,48 @@ class TestZasMask(TestCase):
         self.assertAlmostEqual(1., img.max().item(), places=3, msg="Scaling is wrong.")
 
 
-class TestDecodeRepresentation(TestCase):
-    def setUp(self) -> None:
+class TestDecodeRepresentation:
+
+    @pytest.fixture(scope='class')
+    def offset_rep(self):
+        return OffsetRep((-0.5, 31.5), (-0.5, 31.5), None, (32, 32), cat_output=False)
+
+    @pytest.fixture(scope='class')
+    def em_set(self):
         num_emitter = 100
-        self.em = EmitterSet(torch.rand((num_emitter, 3)) * 40,
-                             torch.rand(num_emitter),
-                             torch.zeros(num_emitter))
+        return EmitterSet(torch.rand((num_emitter, 3)) * 40,
+                          torch.rand(num_emitter),
+                          torch.zeros(num_emitter))
 
-        self.dc = OffsetRep((-0.5, 31.5), (-0.5, 31.5), None, (32, 32), cat_output=False)
+    def test_forward_shape(self, offset_rep, em_set):
 
-    def test_forward_shape(self):
+        p, I, x, y, z = offset_rep.forward(em_set)
+        assert p.shape == I.shape, "Maps of Decode Rep. must have equal dimension."
+        assert I.shape == x.shape, "Maps of Decode Rep. must have equal dimension."
+        assert x.shape == y.shape, "Maps of Decode Rep. must have equal dimension."
+        assert y.shape == z.shape, "Maps of Decode Rep. must have equal dimension."
 
-        p, I, x, y, z = self.dc.forward(self.em)
-        self.assertEqual(p.shape, I.shape, "Maps of Decode Rep. must have equal dimension.")
-        self.assertEqual(I.shape, x.shape, "Maps of Decode Rep. must have equal dimension.")
-        self.assertEqual(x.shape, y.shape, "Maps of Decode Rep. must have equal dimension.")
-        self.assertEqual(y.shape, z.shape, "Maps of Decode Rep. must have equal dimension.")
-
-    def test_forward_equal_nonzeroness(self):
+    def test_forward_equal_nonzeroness(self, offset_rep, em_set):
         """
         Test whether the non-zero entries are the same in all channels.
         Note that this in principle stochastic but the chance of having random float exactly == 0 is super small.
         :return:
         """
-        p, I, x, y, z = self.dc.forward(self.em)
-        self.assertTrue(equal_nonzero(p, I, x, y, z))
+        p, I, x, y, z = offset_rep.forward(em_set)
+        assert equal_nonzero(p, I, x, y, z)
+
+    def test_output_range(self, offset_rep, em_set):
+        """
+        Test whether delta x/y are between -0.5 and 0.5 (which they need to be for 1 coordinate unit == 1px
+        :param offset_rep:
+        :param em_set:
+        :return:
+        """
+        p, I, dx, dy, z = offset_rep.forward(em_set)
+        assert (dx <= 0.5).all(), "delta x/y must be between -0.5 and 0.5"
+        assert (dx >= -0.5).all(), "delta x/y must be between -0.5 and 0.5"
+        assert (dy <= 0.5).all(), "delta x/y must be between -0.5 and 0.5"
+        assert (dy >= -0.5).all(), "delta x/y must be between -0.5 and 0.5"
+
+
 
