91e217a2be6dcfc1b46b0f2210ef3b821ee64306
Lucas-Rapahel Müller <gitdev@LRM.photo>
2019-09-06 12:28:48 +0200

    Add reverse (to convert from adu to photons)

--- deepsmlm/generic/phot_camera.py
+++ deepsmlm/generic/phot_camera.py
@@ -1,15 +1,16 @@
 import torch
 
+import deepsmlm.generic.background
+import deepsmlm.generic.noise
 import deepsmlm.generic.noise as noise
 
 
 class Photon2Camera:
-    def __init__(self, qe, spur_noise, bg_uniform, em_gain, e_per_adu, baseline, read_sigma, bg_uniform_var=0.):
+    def __init__(self, qe, spur_noise, em_gain, e_per_adu, baseline, read_sigma):
         """
 
         :param qe: quantum efficiency
         :param spur_noise: spurious noise
-        :param bg_uniform: uniform background (before poisson)
         :param em_gain: em-gain
         :param e_per_adu: electrons per analog digital unit
         :param baseline: baseline (for non negative values)
@@ -17,14 +18,12 @@ class Photon2Camera:
         """
         self.qe = qe
         self.spur = spur_noise
-        self.bg_uniform = bg_uniform
-        self.bg_uniform_var = bg_uniform_var
         self.em_gain = em_gain
         self.e_per_adu = e_per_adu
         self.baseline = baseline
         self.read_sigma = read_sigma
 
-        self.poisson = noise.Poisson(bg_uniform=0)
+        self.poisson = deepsmlm.generic.noise.Poisson()
         self.gain = noise.Gamma(scale=self.em_gain)
         self.read = noise.Gaussian(sigma_gaussian=self.read_sigma,
                                    bg_uniform=0)
@@ -36,14 +35,9 @@ class Photon2Camera:
         :param param: parameter dictonary
         :return:
         """
-        return Photon2Camera(qe=param['Camera']['qe'],
-                             spur_noise=param['Camera']['spur_noise'],
-                             bg_uniform=param['Simulation']['bg_pois'],
-                             em_gain=param['Camera']['em_gain'],
-                             e_per_adu=param['Camera']['e_per_adu'],
-                             baseline=param['Camera']['baseline'],
-                             read_sigma=param['Camera']['read_sigma'],
-                             bg_uniform_var=param['Simulation']['bg_var'])
+        return Photon2Camera(qe=param['Camera']['qe'], spur_noise=param['Camera']['spur_noise'],
+                             em_gain=param['Camera']['em_gain'], e_per_adu=param['Camera']['e_per_adu'],
+                             baseline=param['Camera']['baseline'], read_sigma=param['Camera']['read_sigma'])
 
     def forward(self, x):
         """
@@ -51,8 +45,10 @@ class Photon2Camera:
         :param x:
         :return:
         """
+        """Clamp input to 0."""
+        x = torch.clamp(x, 0.)
         """Poisson for photon characteristics of emitter (plus autofluorescence etc."""
-        camera = self.poisson.forward((x + self.bg_uniform + (torch.rand(1).item() - 0.5) * self.bg_uniform_var) * self.qe + self.spur)
+        camera = self.poisson.forward(x * self.qe + self.spur)
         """Gamma for EM-Gain (EM-CCD cameras, not sCMOS)"""
         if self.em_gain is not None:
             camera = self.gain.forward(camera)
@@ -64,3 +60,14 @@ class Photon2Camera:
         camera += self.baseline
         camera = torch.max(camera, torch.tensor([0.]))
         return camera
+
+    def reverse(self, x):
+        """
+        Calculate (expected) number of photons
+        :param x:
+        :return:
+        """
+        out = (x - self.baseline) * self.e_per_adu / self.em_gain
+        out -= self.spur
+        out /= self.qe
+        return out
--- deepsmlm/test/test_photcamera.py
+++ deepsmlm/test/test_photcamera.py
@@ -9,12 +9,7 @@ class TestPhotons2Camera:
 
     @pytest.fixture(scope='class')
     def mt1_spec(self):
-        return pc.Photon2Camera(qe=0.9,
-                                bg_uniform=90,
-                                em_gain=300.,
-                                e_per_adu=45.,
-                                baseline=100,
-                                read_sigma=74.4)
+        return pc.Photon2Camera(qe=0.9, spur_noise=, em_gain=300., e_per_adu=45., baseline=100, read_sigma=74.4)
 
     def test_shape(self, mt1_spec):
         x = torch.ones((32, 3, 64, 64))
