c94e4dc97f0dc5a0942d85a5af2d8449cb9ba8d2
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2019-12-15 15:32:25 +0100

    Include sophistacted plots (density vs probability etc.)

--- deepsmlm/evaluation/evaluation.py
+++ deepsmlm/evaluation/evaluation.py
@@ -3,7 +3,10 @@ from matplotlib.ticker import AutoMinorLocator
 from scipy import stats
 import seaborn as sns
 
+import deepsmlm
 import numpy as np
+from joblib import Parallel, delayed
+import random
 import math
 import torch
 
@@ -11,6 +14,20 @@ from deepsmlm.evaluation.metric_library import PrecisionRecallJaccard, RMSEMAD
 import deepsmlm.evaluation.metric_library as metric_lib
 import deepsmlm.generic.emitter as emitter
 from scipy.stats import gaussian_kde
+
+
+def kde_sorted(x, y, plot=False):
+    """
+    Gives a density estimates useful for plotting many datapoints
+    """
+    xy = np.vstack([x, y])
+    z = gaussian_kde(xy)(xy)
+    # Sort the points by density, so that the densest points are plotted last
+    idx = z.argsort()
+    x, y, z = x[idx], y[idx], z[idx]
+    if plot:
+        plt.scatter(x, y, c=z, s=10, edgecolor='', cmap='RdBu_r')
+    return z, x, y
 
 
 class MetricMeter:
@@ -40,24 +57,31 @@ class MetricMeter:
     def avg(self):
         return self.mean
 
-    def hist(self, bins=30, range=None, fit=stats.norm):
+    def hist(self, bins=50, range_hist=None, fit=stats.norm):
         """
 
         :param bins: number of bins
-        :param range: specify range
+        :param range_hist: specify range
         :return: plt.figure
         """
-        if range is not None:
-            bins_ = np.linspace(range[0], range[1], bins + 1)
+        # If no vals, return empty figure
+        if self.vals.numel() <= 10:
+            f = plt.figure()
+            return f
+        elif self.vals.unique().numel() == 1:
+            f = plt.figure()
+            plt.vlines(self.vals[0], 0, 1)
+            return f
+
+        if range_hist is not None:
+            bins_ = np.linspace(range_hist[0], range_hist[1], bins + 1)
         else:
-            bins_ = bins
+            # use 97 percent as range
+            range_hist = np.percentile(self.vals, [1, 99])  # funnily percentile is in percent not 0...1
+            bins_ = np.linspace(*range_hist, bins + 1)
 
         vals = self.vals.view(-1).numpy()
         f, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.1, .9)})
-
-        """If we don't have values return empty figure"""
-        if vals.shape[0] <= 2:
-            return f
 
         """Plot boxplot and distplot."""
         sns.boxplot(vals, ax=ax_box)
@@ -102,7 +126,10 @@ class MetricMeter:
         self.count += 1
 
     def __str__(self):
-        return "(avg) - (sig) = {:.3f} - {:.3f}".format(self.avg, self.std)
+        if self.count >= 2:
+            return "{:.3f} +/- {:.3f}".format(self.avg, self.std)
+        else:
+            return "{:.3f}".format(self.avg)
 
 
 class CumulantMeter(MetricMeter):
@@ -148,6 +175,7 @@ class EvalSet:
         self.dzw = dzw
 
     def __str__(self):
+        # when more than 1 value, print std, otherwise only the estimate
         str = "------------------------ Evaluation Set ------------------------\n"
         str += "Precision {}\n".format(self.prec.__str__())
         str += "Recall {}\n".format(self.rec.__str__())
@@ -175,6 +203,16 @@ class BatchEvaluation:
         self.values = None
         self.px_size = px_size
 
+        # True positives, matched ground truth (tp_match), false positives, false negatives
+        self.tp = None
+        self.tp_match = None
+        self.fp = None
+        self.fn = None
+
+        if self.px_size is not None:
+            if not isinstance(self.px_size, torch.Tensor):
+                self.px_size = torch.Tensor(self.px_size)
+
     def forward(self, output, target):
         """
         Evaluate metrics on a whole batch, i.e. average the stuff. This is based lists of outputs, targets.
@@ -190,9 +228,15 @@ class BatchEvaluation:
         dx, dy, dz, dxw, dyw, dzw = CumulantMeter(), CumulantMeter(), CumulantMeter(), \
                                     CumulantMeter(), CumulantMeter(), CumulantMeter()
 
-        if self.px_size is not None:
-            output = output.convert_em(factor=self.px_size)
-            target = target.convert_em(factor=self.px_size)
+        # Check that both output and target have a unit if we actually have emitters
+        if output.num_emitter >= 1:
+            if output.xy_unit is None or output.xy_unit is None:
+                raise ValueError("For Evaluation both output and target must have units.")
+
+        if output.xy_unit != 'nm':
+            output = output.convert_em(factor=self.px_size, new_xy_unit='nm')
+        if target.xy_unit != 'nm':
+            target = target.convert_em(factor=self.px_size, new_xy_unit='nm')
 
         tp, fp, fn, tp_match = self._matching.forward(output, target)
         prec_, rec_, jaq_ = self._segmentation_eval.forward(tp, fp, fn)
@@ -225,15 +269,158 @@ class BatchEvaluation:
                               mad_vol, mad_lat, mad_axial,
                               dx, dy, dz, dxw, dyw, dzw)
 
-    def plot_sophisticated(self, output, target):
+        self.tp = tp.clone()
+        self.tp_match = tp_match.clone()
+        self.fp = fp.clone()
+        self.fn = fn.clone()
+
+    def plot_sophisticated(self):
         """
         Plots more interesting stuff.
         """
-        if self.px_size is not None:
-            output = output.convert_em(factor=self.px_size)
-            target = target.convert_em(factor=self.px_size)
+        import deepsmlm.generic.plotting.frame_coord as fplot
+        # if self.px_size is not None:
+        #     output_nm = output.convert_em(factor=self.px_size)
+        #     target_nm = target.convert_em(factor=self.px_size)
 
+        # plot errors
+        self.values.dx.hist()
+        plt.xlabel('$x_{pred} - x_{gt}$')
+        plt.show()
 
+        self.values.dy.hist()
+        plt.xlabel('$y_{pred} - y_{gt}$')
+        plt.show()
+
+        self.values.dz.hist()
+        plt.xlabel('$z_{pred} - z_{gt}$')
+        plt.show()
+
+        # plot distances to px centre
+        offsets_x = self.tp.xyz_px[:, 0] - self.tp.xyz_px[:, 0].round()
+        plt.hist(offsets_x, 100)
+        plt.xlabel('dx to px centre')
+        plt.show()
+
+        offsets_y = self.tp.xyz_px[:, 1] - self.tp.xyz_px[:, 1].round()
+        plt.hist(offsets_y, 100)
+        plt.xlabel('dy to px centre')
+        plt.show()
+
+        pm = MetricMeter()
+        pm.vals = self.tp.prob
+        pm.hist(bins=100, range_hist=[0., 1.], fit=None)
+        plt.title('Prob. Distribution of Prediction')
+        plt.xlabel('p')
+        plt.show()
+
+        phm = MetricMeter()
+        phm.vals = self.tp.phot
+        phm.hist(bins=200, range_hist=[0., 1.], fit=None)
+        plt.title('Photon Distribution of Prediction')
+        plt.xlabel('phot')
+        plt.show()
+
+        # plot z over z_gt
+        plt.figure(figsize=(12, 8))
+        z, x, y = kde_sorted(self.tp_match.xyz[:, 2], self.tp.xyz[:, 2], True)
+        plt.plot(x, x, 'r', label='unbiased estimate')
+        plt.legend()
+        plt.xlabel('$z_{gt} [nm]$')
+        plt.ylabel('$z_{pred} [nm]$')
+        plt.title('Distribution of z prediction vs ground truth')
+        plt.show()
+
+        # plot phot over phot_gt
+        plt.figure(figsize=(12, 8))
+        z, x, y = kde_sorted(self.tp_match.phot, self.tp.phot, True)
+        plt.plot(x, x, 'r', label='unbiased estimate')
+        plt.legend()
+        plt.xlabel('$phot_{gt} [nm]$')
+        plt.ylabel('$phot_{pred} [nm]$')
+        plt.title('Distribution of phot prediction vs ground truth')
+        plt.show()
+
+    @staticmethod
+    def plot_sophisticated_raws(raw_output, frame_subset=1000, p_eps=0.05, sim_extent_z=None):
+        """
+        Plots some fancy statistics of the raw outputs. Relatively costly
+        :param: raw_output should be scaled!
+        """
+        if frame_subset is not None:
+            f_all = np.arange(raw_output.size(0))
+            frame_subset = min([frame_subset, raw_output.size(0)])
+            f_subset = random.sample(list(f_all), k=frame_subset)
+        else:
+            f_subset = slice(0, raw_output.size(0))
+
+        ix = raw_output[f_subset, 0] >= p_eps
+        p_values = raw_output[f_subset, 0][ix].reshape(-1)
+        phot_vals = raw_output[f_subset, 1][ix].reshape(-1)
+        dx_vals = raw_output[f_subset, 2][ix].reshape(-1)
+        dy_vals = raw_output[f_subset, 3][ix].reshape(-1)
+        dz_vals = raw_output[f_subset, 4][ix].reshape(-1)
+
+        """MultiProcessing since this KDE thing can take quite some time."""
+        dens_x, pxs, dxs = kde_sorted(p_values, dx_vals)
+        dens_y, pys, dys = kde_sorted(p_values, dy_vals)
+        dens_z, pzs, dzs = kde_sorted(p_values, dz_vals)
+
+        dens_xphot, pxsp, dxsp = kde_sorted(phot_vals, dx_vals)
+        dens_yphot, pysp, dysp = kde_sorted(phot_vals, dy_vals)
+        dens_zphot, pzsp, dzsp = kde_sorted(phot_vals, dz_vals)
+
+        plt.figure(figsize=(24, 12))
+        plt.subplot(131)
+        plt.scatter(pxs, dxs, c=dens_x, s=10, edgecolor='', cmap='RdBu_r')
+        plt.hlines([-0.5, 0.5], 0, 1, 'r', label='px border')
+        plt.xlabel('$p$')
+        plt.ylabel('$dx$')
+        plt.legend()
+        plt.title('Distribution of dx offset vs probability')
+
+        plt.subplot(132)
+        plt.scatter(pys, dys, c=dens_y, s=10, edgecolor='', cmap='RdBu_r')
+        plt.hlines([-0.5, 0.5], 0, 1, 'r', label='px border')
+        plt.xlabel('$p$')
+        plt.ylabel('$dy$')
+        plt.legend()
+        plt.title('Distribution of dy offset vs probability')
+
+        plt.subplot(133)
+        plt.scatter(pzs, dzs, c=dens_z, s=10, edgecolor='', cmap='RdBu_r')
+        if sim_extent_z is not None:
+            plt.hlines(sim_extent_z, 0, 1, 'r', label='simulation extent')
+        plt.xlabel('$p$')
+        plt.ylabel('$z [nm]$')
+        plt.title('Distribution of z offset vs probability')
+        plt.show()
+
+        plt.figure(figsize=(24, 12))
+        plt.subplot(131)
+        plt.scatter(pxsp, dxsp, c=dens_xphot, s=10, edgecolor='', cmap='RdBu_r')
+        plt.hlines([-0.5, 0.5], 0, phot_vals.max().item(), 'r', label='px border')
+        plt.xlabel('$phot$')
+        plt.ylabel('$dx$')
+        plt.legend()
+        plt.title('Distribution of dx offset vs photons')
+
+        plt.subplot(132)
+        plt.scatter(pysp, dysp, c=dens_yphot, s=10, edgecolor='', cmap='RdBu_r')
+        plt.hlines([-0.5, 0.5], 0, phot_vals.max().item(), 'r', label='px border')
+        plt.xlabel('$phot$')
+        plt.ylabel('$dy$')
+        plt.legend()
+        plt.title('Distribution of dy offset vs photons')
+
+        plt.subplot(133)
+        plt.scatter(pzsp, dzsp, c=dens_zphot, s=10, edgecolor='', cmap='RdBu_r')
+        if sim_extent_z is not None:
+            plt.hlines(sim_extent_z, 0, 1, 'r', label='simulation extent')
+        plt.xlabel('$phot$')
+        plt.ylabel('$z [nm]$')
+        plt.title('Distribution of z offset vs photons')
+        plt.show()
 
 
 class SegmentationEvaluation:
