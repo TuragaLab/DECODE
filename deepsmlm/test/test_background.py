231a93962e44a95f0279bf154f7dfb694b6e0075
Lucas-Rapahel Müller <gitdev@LRM.photo>
2020-03-04 10:15:30 +0100

    Update setup.py in travis

--- deepsmlm/test/test_background.py
+++ deepsmlm/test/test_background.py
@@ -7,7 +7,7 @@ from deepsmlm.generic.plotting.frame_coord import PlotFrame
 import deepsmlm.generic.utils.test_utils as tutil
 
 
-class TestBackgroundAbc:
+class TestBackground:
 
     @pytest.fixture(scope='class')
     def bgf(self):
@@ -68,13 +68,13 @@ class TestBackgroundAbc:
         assert (bgf.forward(rframe) >= rframe).all(), "Background is not strictly unsigned additive."
 
 
-class TestOofEmitterBackground(TestBackgroundAbc):
+class TestOofEmitterBackground(TestBackground):
 
     @pytest.fixture(scope='class')
     def bgf(self):
         extent = ((-0.5, 63.5), (-0.5, 63.5), (-750., 750.))
         img_shape = (64, 64)
-        return background.OutOfFocusEmitters(extent[0], extent[1], img_shape, ampl=(1., 1.), num_oof_rg=[1, 2])
+        return background.OutOfFocusEmitters(extent[0], extent[1], img_shape, ampl=(1., 1.), num_oof_rg=(1, 1))
 
     def test_gauss_psf(self, bgf):
         """Tests whether the correct attribute for the gaussian psf is used."""
@@ -87,52 +87,43 @@ class TestOofEmitterBackground(TestBackgroundAbc):
         assert pytest.approx(out.max().item(), 0.01) == 1.
 
 
-class TestPerlinBg(TestBackgroundAbc):
+class TestPerlinBg(TestBackground):
 
     @pytest.fixture(scope='class')
-    def perlin_default(self):
+    def bgf(self):
         img_size = (64, 64)
         return background.PerlinBackground(img_size, 2, 20)
 
-    def test_bypass(self, perlin_default):
+    def test_bypass(self, bgf):
         """
         Tests whether what comes in is what comes out if the amplitude is 0.
         :param perlin_default:
         :return:
         """
-        perlin_default.amplitude = 0.
+        bgf.amplitude = 0.
         x_in = torch.zeros((2, 3, 64, 64))
-        x_out = perlin_default.forward(x_in.clone())
+        x_out = bgf.forward(x_in.clone())
         assert tutil.tens_almeq(x_in, x_out)
 
-    def test_approximate_range(self, perlin_default):
+    def test_approximate_range(self, bgf):
         x_in = torch.zeros((2, 3, 64, 64))
-        x_out = perlin_default.forward(x_in.clone())
+        x_out = bgf.forward(x_in.clone())
         assert x_out.min() >= 0.
-        assert x_out.max() <= perlin_default.amplitude * 1.
+        assert x_out.max() <= bgf.amplitude * 1.
 
-    # @pytest.mark.skip('Test MultiScale only for plotting.')
+
+class TestMultiPerlin(TestBackground):
+
+    @pytest.fixture(scope='class')
+    def
+
+    @pytest.mark.skip_plot
     def test_multiscale(self):
         img_size = (64, 64)
-        cand = background.PerlinBackground.multi_scale_init(img_size, [64, 32, 16, 8], [1, 1, 1, 1],
-                                                            norm_amplitudes=False,
+        cand = background.MultiPerlin(img_size, [64, 32, 16, 8], [1, 1, 1, 1],
+                                                            norm_amps=False,
                                                             draw_amps=True)
         out = cand.forward(torch.zeros((2, 3, 64, 64)))
         PlotFrame(out[0, 0]).plot()
         plt.colorbar()
         plt.show()
-
-@pytest.mark.skip("Only for checking the plotting.")
-def test_nonuniformbg():
-
-    bg_u = background.NonUniformBackground(0.1, img_size=(32, 32))
-    x = torch.rand((2, 3, 32, 32)) * 4
-
-    x_out = bg_u.forward(x)
-
-    plt.figure(figsize=(12, 12))
-    PlotFrame(x[0, 0]).plot()
-    plt.show()
-    plt.figure(figsize=(12, 12))
-    PlotFrame(x_out[0, 0]).plot()
-    plt.show()
