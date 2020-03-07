f7e26f49baeaa13fa82d219da8231393b4a8cadc
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2020-03-07 12:10:42 +0100

    Update and simplify sliceable splitter.

--- deepsmlm/generic/utils/generic.py
+++ deepsmlm/generic/utils/generic.py
@@ -1,8 +1,64 @@
+from deprecated import deprecated
 import numpy as np
 import torch
 
 
 def split_sliceable(x, x_ix: torch.Tensor, ix_low: int, ix_high: int):
+    """
+    Split a sliceable / iterable according to an index into lists of elements between lower and upper bound.
+    Not present elements will be filled with empty instances of the iterable itself.
+
+    This function is mainly used to split the EmitterSet in list of EmitterSets according to its frame index.
+    This function can also be called with arguments x and x_ix being the same. In this case you get a list of indices
+        out which can be used for further indexing.
+
+    Args:
+        x: sliceable / iterable
+        x_ix (torch.Tensor): index according to which to split
+        ix_low (int): lower bound
+        ix_high (int): upper bound
+
+    Returns:
+        x_list: list of instances sliced as specified by the x_ix
+
+    """
+
+    """Sort iterable by x_ix"""
+    x_ix, re = torch.sort(x_ix)
+    x = x[re]
+
+    """
+    arange( + 2) because + 1 for pythonic and another + 1 because the loop before return below goes from 0 to on 
+    range('len' - 1)
+    """
+    picker = np.arange(ix_low, ix_high + 2)
+    ix_sort = np.searchsorted(x_ix, picker)
+
+    return [x[ix_sort[i]:ix_sort[i + 1]] for i in range(ix_sort.shape[0] - 1)]
+
+
+def ix_split(ix: torch.Tensor, ix_min: int, ix_max: int):
+    """
+    Splits an index rather than a sliceable (as above). Might be slower than splitting the sliceable because here we can
+    not just sort once and return the element of interest but must rather return the index.
+
+    Args:
+        ix (torch.Tensor): index to split
+        ix_min (int): lower limit
+        ix_max (int): upper limit (inclusive)
+
+    Returns:
+        list of logical(!) indices
+    """
+    assert ix.dtype in (torch.short, torch.int, torch.long)
+    n = ix_max - ix_min + 1
+
+    log_ix = [ix == ix_c for ix_c in range(ix_min, ix_max + 1)]
+    return log_ix, n
+
+
+@deprecated
+def split_sliceable_old(x, x_ix: torch.Tensor, ix_low: int, ix_high: int):
     """
     Split a sliceable / iterable according to an index into lists of elements between lower and upper bound.
     Not present elements will be filled with empty instances of the iterable itself.
@@ -49,23 +105,3 @@ def split_sliceable(x, x_ix: torch.Tensor, ix_low: int, ix_high: int):
         x_list = x_list + [x_[0:0]] * n_empt
 
     return x_list
-
-
-def ix_split(ix: torch.Tensor, ix_min: int, ix_max: int):
-    """
-    Splits an index rather than a sliceable (as above). Might be slower than splitting the sliceable because here we can
-    not just sort once and return the element of interest but must rather return the index.
-
-    Args:
-        ix (torch.Tensor): index to split
-        ix_min (int): lower limit
-        ix_max (int): upper limit (inclusive)
-
-    Returns:
-        list of logical(!) indices
-    """
-    assert ix.dtype in (torch.short, torch.int, torch.long)
-    n = ix_max - ix_min + 1
-
-    log_ix = [ix == ix_c for ix_c in range(ix_min, ix_max + 1)]
-    return log_ix, n
--- deepsmlm/test/test_utils_generic.py
+++ deepsmlm/test/test_utils_generic.py
@@ -2,52 +2,75 @@ import torch
 import pytest
 
 import deepsmlm.generic.utils.generic as gutils  # candidate
-import deepsmlm.generic.utils.test_utils as tutils
 
 
-def test_split_sliceable():
+class TestSplitSliceable:
 
-    """Setup"""
-    x = torch.rand((5, 3))
-    x_ix = torch.tensor([1, 0, 2, 2, 7])
+    @staticmethod
+    def split_sliceable(x, x_ix, ix_low, ix_high):
+        """
+        Mock function for testing. Slow but works definitely.
 
-    x_1 = torch.rand((0, 3))  # empty tensor
-    x_ix_1 = torch.zeros((0, )).int()
+        Args:
+            x: sliceable / iterable
+            x_ix (torch.Tensor): index according to which to split
+            ix_low (int): lower bound
+            ix_high (int): upper bound
 
-    x_2 = x_ix.clone()  # let sliceable and index be the same tensor
+        Returns:
+            x_list: list of instances sliced as specified by the x_ix
 
-    """Run"""
-    out_0 = gutils.split_sliceable(x, x_ix, 0, 3)
-    out_1 = gutils.split_sliceable(x, x_ix, 0, 0)
-    out_2 = gutils.split_sliceable(x_1, x_ix_1, 0, 0)
-    out_3 = gutils.split_sliceable(x_1, x_ix_1, 0, 1)
-    out_4 = gutils.split_sliceable(x_2, x_2, 0, 3)
-    out_5 = gutils.split_sliceable(x, x_ix, 0, 7)
+        """
 
-    """Tests"""
-    assert len(out_0) == 4
-    assert (out_0[0] == x[1]).all()
-    assert (out_0[1] == x[0]).all()
-    assert (out_0[2] == x[2:4]).all()
-    assert out_0[3].numel() == 0
+        out = []
+        for i in range(ix_low, ix_high + 1):
+            out.append(x[x_ix == i])
 
-    assert len(out_1) == 1
-    assert (out_1[0] == x[1]).all()
+        return out
 
-    assert len(out_2) == 1
-    assert out_2[0].numel() == 0
+    def test_split_sliceable(self):
 
-    assert len(out_3) == 2
-    assert out_3[0].numel() == 0
-    assert out_3[1].numel() == 0
+        """Setup"""
+        x = torch.rand((5, 3))
+        x_ix = torch.tensor([1, 0, 2, 2, 7])
 
-    assert len(out_4) == 4  # Test where sliceable and index are the same tensor
-    assert (out_4[0] == x_ix[1]).all()
-    assert (out_4[1] == x_ix[0]).all()
-    assert (out_4[2] == x_ix[2:4]).all()
+        x_1 = torch.rand((0, 3))  # empty tensor
+        x_ix_1 = torch.zeros((0, )).int()
 
-    assert len(out_5) == 8
-    assert (out_5[-1] == x[-1]).all()
+        x_2 = x_ix.clone()  # let sliceable and index be the same tensor
+
+        """Run"""
+        out_0 = gutils.split_sliceable(x, x_ix, 0, 3)
+        out_1 = gutils.split_sliceable(x, x_ix, 0, 0)
+        out_2 = gutils.split_sliceable(x_1, x_ix_1, 0, 0)
+        out_3 = gutils.split_sliceable(x_1, x_ix_1, 0, 1)
+        out_4 = gutils.split_sliceable(x_2, x_2, 0, 3)
+        out_5 = gutils.split_sliceable(x, x_ix, 0, 7)
+
+        """Tests"""
+        assert len(out_0) == 4
+        assert (out_0[0] == x[1]).all()
+        assert (out_0[1] == x[0]).all()
+        assert (out_0[2] == x[2:4]).all()
+        assert out_0[3].numel() == 0
+
+        assert len(out_1) == 1
+        assert (out_1[0] == x[1]).all()
+
+        assert len(out_2) == 1
+        assert out_2[0].numel() == 0
+
+        assert len(out_3) == 2
+        assert out_3[0].numel() == 0
+        assert out_3[1].numel() == 0
+
+        assert len(out_4) == 4  # Test where sliceable and index are the same tensor
+        assert (out_4[0] == x_ix[1]).all()
+        assert (out_4[1] == x_ix[0]).all()
+        assert (out_4[2] == x_ix[2:4]).all()
+
+        assert len(out_5) == 8
+        assert (out_5[-1] == x[-1]).all()
 
 
 def test_ix_splitting():
