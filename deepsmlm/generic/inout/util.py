c1adb800b702db3bb5303e894ff1339bd75eff11
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2020-04-06 12:02:14 +0200

    Change add root relative to pathlib function.

--- deepsmlm/generic/inout/util.py
+++ deepsmlm/generic/inout/util.py
@@ -1,11 +1,26 @@
-def add_root_relative(path, root):
+from pathlib import Path
+
+
+def add_root_relative(path: (str, Path), root: (str, Path)):
     """
-    Add root if path is relative
-    :param path:
-    :param root:
-    :return:
+    Adds the root to a path if the path is not absolute
+
+    Args:
+        path (str, Path): path to file
+        root (str, Path): root path
+
+    Returns:
+        Path: absolute path to file
+
     """
-    if (path is not None) and (path[0] not in (None, "", "/")):
-        return root + path
-    else:
+    if not isinstance(path, Path):
+        path = Path(path)
+
+    if not isinstance(root, Path):
+        root = Path(root)
+
+    if path.is_absolute():
         return path
+
+    else:
+        return root / path
