343cca54c97a17c0c9c0adce70b23f41e183fd5d
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2020-04-15 17:02:25 +0200

    Refactor processing

--- deepsmlm/generic/utils/processing.py
+++ deepsmlm/generic/utils/processing.py
@@ -12,7 +12,8 @@ class TransformSequence:
             components: components with forward method
             input_slice: list of lists which indicate what is the output to the i-th component; e.g. [[0, 1], [0]]
             means that the first component get's the 0th and 1st element which are input to this instances forward
-            method, the 1st component will get the 0th output of the 0th component.
+            method, the 1st component will get the 0th output of the 0th component. Input slice is ignored when the
+            potential input is not a tuple anyways
         """
         self.com = components
         self._input_slice = input_slice
@@ -21,8 +22,8 @@ class TransformSequence:
         if self._input_slice is not None:
             assert len(self._input_slice) == len(self), "Input slices must be the same number as components"
 
-    @staticmethod
-    def parse(components, param: dict, **kwargs):
+    @classmethod
+    def parse(cls, components, param: dict, **kwargs):
         """
         If all components implemented a parse method, you can do it globally only once for the whole sequence.
 
@@ -32,9 +33,9 @@ class TransformSequence:
             kwargs: arbitrary keyword arguments subject to this class constructor
 
         returns:
-            TransformSequence
+            TransformSequence or subclass of it
         """
-        return TransformSequence([cpt.parse(param) for cpt in components], **kwargs)
+        return cls([cpt.parse(param) for cpt in components], **kwargs)
 
     def __len__(self):
         """
@@ -54,21 +55,24 @@ class TransformSequence:
         """
 
         for i, com in enumerate(self.com):
-            if self._input_slice is not None:
-                com_in = itemgetter(*self._input_slice[i])(x)  # get specific outputs as input for next com
-                if len(self._input_slice[i]) >= 2:
-                    x = com.forward(*com_in)
+            if isinstance(x, tuple):
+                if self._input_slice is not None:
+                    com_in = itemgetter(*self._input_slice[i])(x)  # get specific outputs as input for next com
+                    if len(self._input_slice[i]) >= 2:
+                        x = com.forward(*com_in)
+                    else:
+                        x = com.forward(com_in)
                 else:
-                    x = com.forward(com_in)
+                    x = com.forward(*x)
             else:
-                x = com.forward(*x)
+                x = com.forward(x)
 
         return x
 
 
 class ParallelTransformSequence(TransformSequence):
     """
-    Simple processing class that forwards data through all of it's components parallely (not in a hardware sense) and
+    Simple processing class that forwards data through all of it's components parallelly (not in a hardware sense) and
     returns a list of the output or combines them if a merger is specified.
     """
     def __init__(self, components, input_slice, merger=None):
@@ -93,3 +97,17 @@ class ParallelTransformSequence(TransformSequence):
             return self.merger(out_cache)
         else:
             return out_cache
+
+
+class Identity:
+    """Return what came in."""
+    @staticmethod
+    def parse(param):
+        return Identity()
+
+    @staticmethod
+    def forward(*x):
+        if len(x) == 1:
+            return x[0]
+        return x
+
