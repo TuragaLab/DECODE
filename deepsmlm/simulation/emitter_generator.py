a64c548da3fd17632e419e9c4e4b647af836f22d
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2020-04-22 21:14:07 +0200

    Simplify emitter number search.

--- deepsmlm/simulation/emitter_generator.py
+++ deepsmlm/simulation/emitter_generator.py
@@ -1,12 +1,10 @@
-from abc import ABC, abstractmethod  # abstract class
-import math
-import numpy as np
-import random
-from random import randint
 import warnings
+from abc import ABC, abstractmethod  # abstract class
+
+import numpy as np
+import time
 import torch
 from torch.distributions.exponential import Exponential
-
 
 from deepsmlm.generic.emitter import EmitterSet, LooseEmitterSet
 
@@ -29,13 +27,15 @@ class EmitterPopperSingle(EmitterPopperABC):
     Simple Emitter sampler. Samples emitters from a structure
     """
 
-    def __init__(self, *, structure, photon_range: tuple, xy_unit, density: float = None, emitter_av: float = None):
+    def __init__(self, *, structure, photon_range: tuple, xy_unit, px_size, density: float = None,
+                 emitter_av: float = None):
         super().__init__()
 
         self.structure = structure
         self.density = density
         self.photon_range = photon_range
         self.xy_unit = xy_unit
+        self.px_size = px_size
 
         """
         Sanity Checks.
@@ -44,11 +44,11 @@ class EmitterPopperSingle(EmitterPopperABC):
         if (density is None and emitter_av is None) or (density is not None and emitter_av is not None):
             raise ValueError("You must XOR parse either density or emitter average. Not both or none.")
 
+        self.area = self.structure.area
+
         if emitter_av is not None:
-            self.area = self.structure.area
             self.emitter_av = emitter_av
         else:
-            self.area = self.structure.area
             self.emitter_av = self.density * self.area
 
     def pop(self) -> EmitterSet:
@@ -62,111 +62,160 @@ class EmitterPopperSingle(EmitterPopperABC):
         n = np.random.poisson(lam=self.emitter_av)
 
         xyz = self.structure.pop(n, 3)
-        phot = torch.randint(*self.photon_range, (n, ))
+        phot = torch.randint(*self.photon_range, (n,))
         frame_ix = torch.zeros_like(phot)
 
         return EmitterSet(xyz=xyz,
                           phot=phot,
                           frame_ix=frame_ix,
                           id=None,
-                          xy_unit=self.xy_unit)
+                          xy_unit=self.xy_unit,
+                          px_size=self.px_size)
 
 
 class EmitterPopperMultiFrame(EmitterPopperSingle):
-    def __init__(self, *, structure, intensity_mu_sig, lifetime, xy_unit, num_frames=3, density=None, emitter_av=None,
-                 intensity_th=None):
+    def __init__(self, *, structure, intensity_mu_sig, lifetime, xy_unit, px_size, frames: (int, tuple) = 3,
+                 density=None, emitter_av=None, intensity_th=None):
         """
 
-        :param structure: structure to sample locations frame
-        :param density: density of fluophores
-        :param intensity_mu_sig: intensity parametrisation for gaussian dist (mu, sig)
-        :param lifetime: average lifetime
-        :param num_frames: number of frames
-        :param emitter_av: average number of emitters (note that this overrides the density)
-        :param intensity_th: defines the minimal intensity
+        Args:
+            structure:
+            intensity_mu_sig:
+            lifetime:
+            xy_unit:
+            px_size:
+            frames (int, tuple): if int, it specifies the number of frames, if tuple it specifies the frame range
+            density:
+            emitter_av:
+            intensity_th:
         """
         super().__init__(structure=structure,
                          photon_range=None,
                          xy_unit=xy_unit,
+                         px_size=px_size,
                          density=density,
                          emitter_av=emitter_av)
 
-        self.num_frames = num_frames
+        self.frames = frames
         self.intensity_mu_sig = intensity_mu_sig
         self.intensity_dist = torch.distributions.normal.Normal(self.intensity_mu_sig[0],
                                                                 self.intensity_mu_sig[1])
-        self.intensity_th = intensity_th if intensity_th is not None else 0.0
+        self.intensity_th = intensity_th if intensity_th is not None else 1e-8
         self.lifetime_avg = lifetime
         self.lifetime_dist = Exponential(1 / self.lifetime_avg)  # parse the rate not the scale ...
-        if num_frames != 3:
-            raise ValueError("Current emitter generator needs to be changed to allow for more than 3 frames.")
 
-        self.frame_range = (int(-(self.num_frames - 1) / 2), int((self.num_frames - 1) / 2))
-
-        self.t0_dist = torch.distributions.uniform.Uniform(self.frame_range[0] - 3 * self.lifetime_avg,
-                                                           self.frame_range[1] + 3 * self.lifetime_avg)
-
-        """Determine the number of emitters. Depends on lifetime and num_frames. Rough estimate."""
-        # self.emitter_av = math.ceil(self.emitter_av * 2 * self.num_frames / (1))
-        self.emitter_av = emitter_av
-        self._emitter_av_total = None
+        self.t0_dist = torch.distributions.uniform.Uniform(*self.frame_range_plus)
 
         """
+        Determine the total number of emitters. Depends on lifetime and num_frames. 
         Search for the actual value of total emitters on the extended frame range so that on the 0th frame we have
         as many as we have specified in self.emitter_av
         """
-        self._total_emitter_average_search()
+        self._emitter_av_total = None
+        self._emitter_av_total = self._total_emitter_average_search()
 
-    def gen_loose_emitter(self):
-        """
-        Generate a loose emitterset (float starting time ...)
-        :return: isntance of LooseEmitterset
-        """
-        """Pop a multi_frame emitter set."""
-        if self._emitter_av_total is None:
-            lam_in = self.emitter_av
-        else:
-            lam_in = self._emitter_av_total
+        """Sanity"""
+        if self.num_frames != 3 or self.frame_range != (-1, 1):
+            warnings.warn("Not yet tested number of frames / frame range.")
 
-        n = np.random.poisson(lam=lam_in)
+    @property
+    def frame_range(self):
+        if isinstance(self.frames, int):
+            return int(-(self.frames - 1) / 2), int((self.frames - 1) / 2)
+
+        elif isinstance(self.frames, (tuple, list)):
+            return self.frames
+
+    @property
+    def frame_range_plus(self):
+        """
+        Frame range including buffer in front and end to account for build up effects.
+
+        """
+        return self.frame_range[0] - 3 * self.lifetime_avg, self.frame_range[1] + 3 * self.lifetime_avg
+
+    @property
+    def num_frames(self):
+        if isinstance(self.frames, int):
+            return self.frames
+
+        elif isinstance(self.frames, (tuple, list)):
+            return self.frames[1] - self.frames[0] + 1
+
+    @property
+    def num_frames_plus(self):
+        return self.frame_range_plus[1] - self.frame_range_plus[0] + 1
+
+    @classmethod
+    def parse(cls, param, structure, frames: (int, tuple)):
+        return cls(structure=structure,
+                   intensity_mu_sig=param.Simulation.intensity_mu_sig,
+                   lifetime=param.Simulation.lifetime_avg,
+                   xy_unit=param.Simulation.xy_unit,
+                   px_size=param.Camera.px_size,
+                   frames=frames,
+                   density=param.Simulation.density,
+                   emitter_av=param.Simulation.emitter_av,
+                   intensity_th=param.Simulation.intensity_th)
+
+    def _test_actual_number(self, num_em) -> int:
+        """
+        Test actual number of emitters per frame
+
+        Returns:
+            int: number of emitters on a target frame
+        """
+        return len(self.gen_loose_emitter(num_em).return_emitterset().get_subset_frame(*self.frame_range)) / self.num_frames
+
+    def _total_emitter_average_search(self, n: int = 100000):
+        """
+        Search for the correct total emitter average of loose emitters so that one results in the correct number of
+        emitters per frame.
+
+        Args:
+            n (int): input samples to test
+
+        Returns:
+            number of actual emitters to put in random distribution to get specified number of emitters per frame
+
+        """
+
+        """
+        Measure for a significantly large number of emitters and then use rule of proportion to get the correct
+        value. An analytical formula would be nice but this is a way to solve the problem ...
+        """
+
+        out = self._test_actual_number(n)
+        return n / out * self.emitter_av
+
+    def gen_loose_emitter(self, num_em):
+        """
+        Generate loose EmitterSet
+
+        Returns:
+            LooseEmitterSet
+        """
+
+        n = np.random.poisson(lam=num_em)
         xyz = self.structure.pop(n, 3)
 
         """Draw from intensity distribution but clamp the value so as not to fall below 0."""
         intensity = torch.clamp(self.intensity_dist.sample((n,)), self.intensity_th, None)
 
         """Distribute emitters in time. Increase the range a bit."""
-        t0 = self.t0_dist.sample((n, ))
+        t0 = self.t0_dist.sample((n,))
         ontime = self.lifetime_dist.rsample((n,))
 
-        return LooseEmitterSet(xyz, intensity, ontime, t0, xy_unit=self.xy_unit, id=None)
-
-    def _test_actual_number(self):
-        """
-        Function to test what the actual number of emitters on the target frame is. Basically for debugging.
-        :return: number of emitters on 0th frame.
-        """
-        return len(self.gen_loose_emitter().return_emitterset().get_subset_frame(0, 0))
-
-    def _total_emitter_average_search(self):
-        """
-        Search for the correct total emitter average (since the users specifies it on the 0th frame but we have more)
-        :return:
-        """
-        actual_emitters = torch.zeros(50)
-        for i in range(actual_emitters.shape[0]):
-            actual_emitters[i] = self._test_actual_number()
-        actual_emitters = actual_emitters.mean().item()
-        self._emitter_av_total = self.emitter_av ** 2 / actual_emitters
+        return LooseEmitterSet(xyz, intensity, ontime, t0, xy_unit=self.xy_unit, px_size=self.px_size, id=None)
 
     def pop(self):
         """
-        Return Emitters with frame index. Use subset of the originally increased range of frames because of
-        statistical reasons. Shift index to -1, 0, 1 ...
-        :return EmitterSet
+        Return sampled EmitterSet in specified frame range.
+
+        Returns:
+            EmitterSet
         """
 
-        loose_em = self.gen_loose_emitter()
-
-        emset = loose_em.return_emitterset().get_subset_frame(-1, 1)
-        emset.xy_unit = 'px'
-        return emset
+        loose_em = self.gen_loose_emitter(num_em=self._emitter_av_total)
+        em = loose_em.return_emitterset()
+        return em.get_subset_frame(*self.frame_range)  # because the simulated frame range is larger
--- deepsmlm/test/test_emittergenerator.py
+++ deepsmlm/test/test_emittergenerator.py
@@ -1,8 +1,11 @@
+import numpy as np
 import pytest
 import torch
 
-import deepsmlm.simulation.emitter_generator as emgen
 import deepsmlm.generic.emitter as em
+import deepsmlm.simulation.emitter_generator as emgen
+from deepsmlm.generic.utils import test_utils
+from deepsmlm.simulation.structure_prior import RandomStructure
 
 
 class TestEmitterPopperABC:
@@ -33,17 +36,18 @@ class TestEmitterPopperABC:
 
 class TestEmitterPopper(TestEmitterPopperABC):
 
+    @pytest.fixture()
+    def structure(self):
+        return RandomStructure((10., 20.), (30., 40.), (1000, 2000.))
+
     @pytest.fixture(params=[[None, 10.], [2., None]], ids=["em_av", "dens"])
-    def em_pop(self, request):
-        from deepsmlm.simulation.structure_prior import RandomStructure
+    def em_pop(self, request, structure):
 
         dens, em_av = request.param  # unpack arguments
-
-        structure = RandomStructure((10., 20.), (30., 40.), (1000, 2000.))
-
         cand = emgen.EmitterPopperSingle(structure=structure,
                                          photon_range=(100, 2000),
                                          xy_unit='px',
+                                         px_size=(1., 1.),
                                          density=dens,
                                          emitter_av=em_av)
 
@@ -70,39 +74,72 @@ class TestEmitterPopper(TestEmitterPopperABC):
         em_av_out = torch.tensor(em_av_out).float()
 
         """Assert"""
-        assert em_av_out.mean() == pytest.approx(exp_av, exp_av/10), "Average seems to be off."
+        assert em_av_out.mean() == pytest.approx(exp_av, exp_av / 10), "Average seems to be off."
 
     def test_frame_ix(self, em_pop):
         """Make sure that the frame_ix is 0."""
 
         """Run and Test"""
-        n = 10
+        n = 100
         for _ in range(n):
             assert (em_pop().frame_ix == 0).all()
 
 
-@pytest.mark.skip("Deprecation.")
-class TestEmitterPopperMultiFrame:
+class TestEmitterPopperMultiframe(TestEmitterPopper):
 
-    @pytest.fixture(scope='class')
-    def empopper(self):
-        structure_prior = RandomStructure((-0.5, 63.5),
-                                          (-0.5, 63.5),
-                                          (-750., 750.))
+    @pytest.fixture(params=[[None, 10.], [2., None]], ids=["em_av", "dens"])
+    def em_pop(self, request, structure):
+        dens, em_av = request.param  # unpack arguments
+        cand = emgen.EmitterPopperMultiFrame(structure=structure,
+                                             intensity_mu_sig=(100, 2000),
+                                             xy_unit='px',
+                                             px_size=(1., 1.),
+                                             lifetime=2.,
+                                             density=dens,
+                                             emitter_av=em_av)
 
-        return emgen.EmitterPopperMultiFrame(structure=structure_prior,
-                                             density=None,
-                                             intensity_mu_sig=(1000., 300.),
-                                             lifetime=1.5,
-                                             num_frames=3.,
-                                             emitter_av=60)
+        return cand
 
-    def test_loose_emset(self, empopper):
-        loose_em = empopper.gen_loose_emitter()
-        assert loose_em.intensity.mean().item() == pytest.approx(1000., 0.1)
-        assert loose_em.intensity.std().item() == pytest.approx(300., 0.1)
+    def test_frame_ix(self, em_pop):
+        """Run and Test"""
+        n = 100
+        for _ in range(n):
+            assert (em_pop().frame_ix.unique() == torch.tensor([-1, 0, 1])).all()
 
-    def test_emset_after_distribution(self, empopper):
-        emset = empopper.pop()
-        assert emset.phot.max().item() <= 1000 + 5 * empopper.intensity_mu_sig[1]
-        assert emset.phot.min().item() >= 0
+    def test_frame_specification(self, structure):
+
+        generator = emgen.EmitterPopperMultiFrame(structure=structure,
+                                      intensity_mu_sig=(100, 2000),
+                                      xy_unit='px',
+                                      px_size=(1., 1.),
+                                      lifetime=2.,
+                                      emitter_av=100,
+                                      frames=(-100, 100))
+
+        generator.pop()
+
+    def test_uniformity(self, structure):
+        """
+        Tests whether there are approx. equal amount of fluorophores on all frames.
+        Tested with a high number for statistical reasons. This test can fail by statistical means.
+        """
+
+        """Setup"""
+        em_gen = emgen.EmitterPopperMultiFrame(structure=structure,
+                                               intensity_mu_sig=(100, 2000),
+                                               xy_unit='px',
+                                               px_size=(1., 1.),
+                                               lifetime=2.,
+                                               density=None,
+                                               emitter_av=10000,
+                                               frames=(0, 1000))
+
+        """Run"""
+        emitters = em_gen.pop()
+
+        """Asserts"""
+        bin_count, _ = np.histogram(emitters.frame_ix, bins=np.arange(1002))
+        bin_count = torch.from_numpy(bin_count)
+
+        assert test_utils.tens_almeq(bin_count, torch.ones_like(bin_count) * 10000, 2000)  # plus minus 1000
+        assert bin_count.float().mean() == pytest.approx(10000, rel=0.05)
