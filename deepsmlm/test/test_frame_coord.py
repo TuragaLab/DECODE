5b3002110d787157bb4afa405edf06dbdf137f6c
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2020-04-23 13:07:12 +0200

    Plot matching in 3D

---
+++ deepsmlm/test/test_frame_coord.py
@@ -0,0 +1,17 @@
+import torch
+import matplotlib.pyplot as plt
+
+import pytest
+
+from deepsmlm.generic.plotting import frame_coord
+from deepsmlm.generic.emitter import EmptyEmitterSet, RandomEmitterSet
+
+
+# @pytest.mark.plot
+@pytest.mark.parametrize("em_tar", [torch.zeros((0, 3)), torch.rand((50, 3))])
+@pytest.mark.parametrize("em_out", [torch.zeros((0, 3)), torch.rand((50, 3))])
+def test_plot3d(em_tar, em_out):
+
+    f = plt.figure()
+    frame_coord.PlotCoordinates3D(pos_tar=em_tar, pos_out=em_out).plot()
+    plt.show()
