0461248fa5cbab449b57c7107cc68872808348b9
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2020-05-06 11:35:49 +0200

    Remove old target generators

--- deepsmlm/neuralfitter/target_generator.py
+++ deepsmlm/neuralfitter/target_generator.py
@@ -1,13 +1,9 @@
 from abc import ABC, abstractmethod
 
-import deprecated
 import torch
-from sklearn import neighbors
-from torch.nn import functional
 
 from deepsmlm.generic import EmitterSet
-from deepsmlm.generic.coordinate_trafo import A2BTransform
-from deepsmlm.simulation.psf_kernel import PSF, DeltaPSF
+from deepsmlm.simulation.psf_kernel import DeltaPSF
 
 
 class TargetGenerator(ABC):
@@ -36,7 +32,20 @@ class TargetGenerator(ABC):
 
     @abstractmethod
     def forward_(self, xyz: torch.Tensor, phot: torch.Tensor, frame_ix: torch.Tensor,
-                 ix_low: int = None, ix_high: int = None):
+                 ix_low: int = None, ix_high: int = None) -> torch.Tensor:
+        """
+
+        Args:
+            xyz: Coordinates
+            phot: Photon values
+            frame_ix: Frame index
+            ix_low: lower frame index bound
+            ix_high: upper frame index bound
+
+        Returns:
+            torch.Tensor
+
+        """
 
         raise NotImplementedError
 
@@ -45,8 +54,8 @@ class TargetGenerator(ABC):
 
         Args:
             em (EmitterSet): EmitterSet. Defaults to xyz_nm coordinates.
-            ix_low (int): lower frame index
-            ix_high (int): upper frame index
+            ix_low (int): lower frame index bound
+            ix_high (int): upper frame index bound
 
         Returns:
             tar (torch.Tensor): Target.
@@ -144,7 +153,7 @@ class UnifiedEmbeddingTarget(TargetGenerator):
         return xy_tar
 
     def forward_(self, xyz: torch.Tensor, phot: torch.Tensor, frame_ix: torch.Tensor,
-                 ix_low: int, ix_high: int):
+                 ix_low: int, ix_high: int) -> torch.Tensor:
 
         """Get index of central bin for each emitter, throw out emitters that are out of the frame."""
         mask, ix = self._get_central_px(xyz, frame_ix)
@@ -173,283 +182,4 @@ class UnifiedEmbeddingTarget(TargetGenerator):
         if bg is not None:
             target = torch.cat((target, bg.unsqueeze(1)), 1)
 
-        return target
-
-
-@deprecated.deprecated("New version UnifiedEmbedding.")
-class SpatialEmbedding(DeltaPSF):
-    """
-    Compute Spatial Embedding.
-    """
-
-    def __init__(self, xextent: tuple, yextent: tuple, img_shape: tuple):
-        """
-
-        Args:
-            xextent (tuple): extent of the frame in x
-            yextent (tuple): extent of the frame in y
-            img_shape (tuple): img shape
-        """
-        super().__init__(xextent=xextent, yextent=yextent, img_shape=img_shape)
-
-        """Setup the bin centers x and y"""
-        self._bin_ctr_x = (0.5 * (self._bin_x[1] + self._bin_x[0]) - self._bin_x[0] + self._bin_x)[:-1]
-        self._bin_ctr_y = (0.5 * (self._bin_y[1] + self._bin_y[0]) - self._bin_y[0] + self._bin_y)[:-1]
-
-        self._offset_max_x = self._bin_x[1] - self._bin_ctr_x[0]
-        self._offset_max_y = self._bin_y[1] - self._bin_ctr_y[0]
-
-    def forward_(self, xyz: torch.Tensor, frame_ix: torch.Tensor, ix_low=None, ix_high=None):
-        """
-
-        Args:
-            xyz: coordinates of size N x (2 or 3)
-            frame_ix: (optional) frame index
-            ix_low: (optional) lower frame_index, if None will be determined automatically
-            ix_high: (optional) upper frame_index, if None will be determined automatically
-
-        Returns:
-
-        """
-        xyz, weight, frame_ix, ix_low, ix_high = PSF.forward(self, xyz, None, frame_ix, ix_low, ix_high)
-
-        mask = self._fov_filter.clean_emitter(xyz)
-        n_ix, x_ix, y_ix = self.px_search(xyz[mask], frame_ix[mask].long())
-
-        xy_offset = torch.zeros((ix_high - ix_low + 1, 2, *self.img_shape))
-        xy_offset[n_ix, 0, x_ix, y_ix] = xyz[mask, 0] - self._bin_ctr_x[x_ix]
-        xy_offset[n_ix, 1, x_ix, y_ix] = xyz[mask, 1] - self._bin_ctr_y[y_ix]
-
-        return xy_offset
-
-    def forward(self, em: EmitterSet, ix_low: int = None, ix_high: int = None):
-        return self.forward_(em.xyz_px, em.frame_ix, ix_low, ix_high)
-
-
-@deprecated.deprecated("New version UnifiedEmbedding.")
-class SinglePxEmbedding(TargetGenerator):
-    """
-    Generate binary target and embeddings of coordinates and photons.
-    """
-
-    def __init__(self, xextent: tuple, yextent: tuple, img_shape: tuple):
-        """
-
-        Args:
-            xextent (tuple): extent of the target map in x
-            yextent (tuple): extent of the target map in y
-            img_shape (tuple): image shape
-        """
-
-        super().__init__(unit='px')
-
-        self._delta = DeltaPSF(xextent,
-                               yextent,
-                               img_shape)
-
-        self._offset = SpatialEmbedding(xextent, yextent, img_shape)
-
-    def forward_(self, xyz: torch.Tensor, phot: torch.Tensor, frame_ix: torch.Tensor,
-                 ix_low: int = None, ix_high: int = None):
-        """
-        Forward through target generator.
-
-        Args:
-            xyz (torch.Tensor): coordinates. Dimension N x 3
-            phot (torch.Tensor): photon count. Dimension N
-            frame_ix (torch.Tensor(int)): frame_index. Dimension N
-            ix_low (int): lower bound for frame_ix
-            ix_high (int): upper bound for frame_ix
-
-        Returns:
-            target (torch.Tensor): Target frames. Dimension F x 5 x H x W, where F are the frames.
-        """
-
-        # mask = self._delta._fov_filter.clean_emitter(xyz)
-        # xyz, phot, frame_ix = xyz[mask], phot[mask], frame_ix[mask]
-        #
-        # n_ix, x_ix, y_ix = self._delta.px_search(xyz, frame_ix.long())
-        #
-        # embedding = torch.zeros((ix_high - ix_low + 1, 5, *self.img_shape))
-        # embedding[n_ix, 0, x_ix, y_ix] =
-
-        p_map = self._delta.forward(xyz, torch.ones_like(xyz[:, 0]), frame_ix, ix_low, ix_high)
-        phot_map = self._delta.forward(xyz, phot, frame_ix, ix_low, ix_high)
-        xy_map = self._offset.forward_(xyz, frame_ix=frame_ix, ix_low=ix_low, ix_high=ix_high)
-        z_map = self._delta.forward(xyz, weight=xyz[:, 2], frame_ix=frame_ix, ix_low=ix_low, ix_high=ix_high)
-
-        return torch.cat((p_map.unsqueeze(1),
-                          phot_map.unsqueeze(1),
-                          xy_map,
-                          z_map.unsqueeze(1)), 1)
-
-
-@deprecated.deprecated("New version UnifiedEmbedding.")
-class KernelEmbedding(SinglePxEmbedding):
-    """
-    Generate a target with ROI wise embedding (kernel).
-
-    Attributes:
-        roi_size (int): size of the ROI in which we define a target
-    """
-
-    def __init__(self, xextent, yextent, img_shape, roi_size=3):
-        super().__init__(xextent, yextent, img_shape)
-        self._xextent = xextent
-        self._yextent = yextent
-        self._img_shape = img_shape
-
-        self.roi_size = roi_size
-
-        """Addition 'kernel' for phot channel, dxyz"""
-        dx = (self._xextent[1] - self._xextent[0]) / self._img_shape[0]
-        dy = (self._yextent[1] - self._xextent[0]) / self._img_shape[1]
-        self._kern_dx = torch.tensor([[dx, dx, dx], [0., 0., 0.], [-dx, -dx, -dx]])
-        self._kern_dy = torch.tensor([[dy, 0., -dy], [dy, 0., -dy], [dy, 0., -dy]])
-
-        """Sanity checks."""
-        if self.roi_size != 3:
-            raise NotImplementedError("Currently only ROI size 3 is implemented and tested.")
-
-    @staticmethod
-    def parse(param):
-        return KernelEmbedding(xextent=param.Simulation.psf_extent[0],
-                               yextent=param.Simulation.psf_extent[1],
-                               img_shape=param.Simulation.img_size,
-                               roi_size=param.HyperParameter.target_roi_size)
-
-    def _roi_increaser(self, offset_target):
-        """
-
-        Args:
-            xyz:
-            phot:
-            frame_ix:
-
-        Returns:
-
-        """
-
-        # zero pad the target in image space so that we don't have border problems
-        offset_target_pad = functional.pad(offset_target, [1, 1, 1, 1], mode='constant', value=0.)
-        target = torch.zeros_like(offset_target_pad)
-
-        is_emitter = offset_target_pad[0].nonzero()
-        # loop over the non-zero elements
-        for i in range(is_emitter.size(0)):
-            ix_x = slice(is_emitter[i, 0].item() - 1, is_emitter[i, 0].item() + 2)
-            ix_y = slice(is_emitter[i, 1].item() - 1, is_emitter[i, 1].item() + 2)
-
-            """
-            Leave p channel unchanged. For the other channels increase the target.
-            That means increase target in phot and z channel that we replicate the value (roi-wise constant).
-            For x and y we make pointers.
-            """
-            target[1, ix_x, ix_y] = offset_target_pad[1, is_emitter[i, 0], is_emitter[i, 1]]
-            target[2, ix_x, ix_y] = self._kern_dx + offset_target_pad[2, is_emitter[i, 0], is_emitter[i, 1]]
-            target[3, ix_x, ix_y] = self._kern_dy + offset_target_pad[3, is_emitter[i, 0], is_emitter[i, 1]]
-            target[4, ix_x, ix_y] = offset_target_pad[4, is_emitter[i, 0], is_emitter[i, 1]]
-
-        """Set px centres to original value and remove padding."""
-        target[:, is_emitter[:, 0], is_emitter[:, 1]] = offset_target_pad[:, is_emitter[:, 0], is_emitter[:, 1]]
-        target = target[:, 1:-1, 1:-1]
-
-        return target
-
-    def forward_(self, xyz: torch.Tensor, phot: torch.Tensor, frame_ix: torch.Tensor,
-                 ix_low: int = None, ix_high: int = None):
-
-        offset_target = super().forward_(xyz, phot, frame_ix, ix_low, ix_high)
-        for i in range(offset_target.size(0)):
-            offset_target[i] = self._roi_increaser(offset_target[i])
-
-        return offset_target
-
-
-@deprecated.deprecated("Tried some time ago but never used. Probably not up to date. Not tested.")
-class GlobalOffsetRep(SinglePxEmbedding):
-    def __init__(self, xextent, yextent, zextent, img_shape, photon_threshold=None, masked=False):
-        super().__init__(xextent, yextent, zextent, img_shape, cat_output=True, photon_threshold=photon_threshold)
-        self.xextent = xextent
-        self.yextent = yextent
-        self.zextent = zextent
-        self.img_shape = img_shape
-        self.masked = masked
-
-        self.nearest_neighbor = neighbors.KNeighborsClassifier(1, weights='uniform')
-
-        """Grid coordinates"""
-        # ToDo: Double check
-        x = torch.linspace(xextent[0], xextent[1], steps=img_shape[0] + 1).float()[:-1]
-        y = torch.linspace(yextent[0], yextent[1], steps=img_shape[1] + 1).float()[:-1]
-
-        # add half a pixel to get the center
-        x += (x[1] - x[0]) / 2
-        y += (y[1] - y[0]) / 2
-        self.x_grid, self.y_grid = torch.meshgrid(x, y)
-        self.xy_list = torch.cat((self.x_grid.contiguous().view(-1, 1), self.y_grid.contiguous().view(-1, 1)), dim=1)
-
-    def assign_emitter(self, x, coordinates=None):
-        """
-        Assign px to the closest emitter
-        :param x: emitter instance
-        :return:
-        """
-        """Fit the emitters. Fit only in 2D"""
-        dummy_index = torch.arange(0, x.num_emitter)
-        self.nearest_neighbor.fit(x.xyz[:, :2].numpy(), dummy_index)
-
-        """Predict NN for all image coordinates"""
-        if coordinates is None:
-            pred_ix = self.nearest_neighbor.predict(self.xy_list)
-        else:
-            pred_ix = self.nearest_neighbor.predict(coordinates)
-
-        return torch.from_numpy(pred_ix)
-
-    def forward_(self, x):
-        """
-
-        :param x: emitter instance
-        :return: 5 ch output
-        """
-        p_map = self._delta.forward(x, torch.ones_like(x.phot))
-        p_map[p_map > 1] = 1
-
-        """If masked: Do not output phot / dx, dy, dz everywhere but just around an emitter."""
-        if self.masked:
-            conv_kernel = torch.ones((1, 1, 3, 3))
-            photxyz_mask = functional.conv2d(p_map.unsqueeze(0), conv_kernel, padding=1).squeeze(0).squeeze(0)
-            photxyz_mask[photxyz_mask >= 1.] = 1
-
-            img_pseudo_extent = ((-0.5, self.img_shape[0] - 0.5), (-0.5, self.img_shape[1] - 0.5))
-            # find indices
-            mat_indices = photxyz_mask.squeeze(0).nonzero()
-            coordinates = A2BTransform(img_pseudo_extent, (self.xextent, self.yextent)).a2b(mat_indices.float())
-
-            pred_ix = self.assign_emitter(x, coordinates)
-            pred_image = (-1) * torch.ones((self.img_shape[0], self.img_shape[1]))
-            pred_image[photxyz_mask.byte()] = pred_ix.float()
-
-        else:
-            pred_ix = self.assign_emitter(x, None)
-            pred_image = pred_ix.reshape(self.img_shape[0], self.img_shape[1])
-            photxyz_mask = torch.ones_like(pred_image)
-
-        """Calculate l1 distance per dimension to predicted index for all image coordinates"""
-        dx = torch.zeros(self.img_shape[0], self.img_shape[1])
-        dy = torch.zeros_like(dx)
-        z = torch.zeros_like(dx)
-        phot_map = torch.zeros_like(dx)
-
-        for i in range(x.num_emitter):
-            mask = (pred_image == i)
-
-            dx[mask] = x.xyz[i, 0] - self.x_grid[mask]
-            dy[mask] = x.xyz[i, 1] - self.y_grid[mask]
-            z[mask] = x.xyz[i, 2]
-            phot_map[mask] = x.phot[i]
-
-        target = torch.cat((p_map, phot_map.unsqueeze(0), dx.unsqueeze(0), dy.unsqueeze(0), z.unsqueeze(0)), 0)
-        target[1:] *= photxyz_mask.float()
         return target
--- deepsmlm/test/test_targetgenerator.py
+++ deepsmlm/test/test_targetgenerator.py
@@ -1,14 +1,10 @@
 import pytest
 import torch
-from matplotlib import pyplot as plt
 
 import deepsmlm.simulation.psf_kernel as psf_kernel
 from deepsmlm.generic import EmitterSet, CoordinateOnlyEmitter, RandomEmitterSet, EmptyEmitterSet
-from deepsmlm.generic.plotting.frame_coord import PlotFrame, PlotFrameCoord
 from deepsmlm.generic.utils import test_utils as tutil
-from deepsmlm.generic.utils.test_utils import equal_nonzero
 from deepsmlm.neuralfitter import target_generator
-from deepsmlm.neuralfitter.target_generator import SinglePxEmbedding, KernelEmbedding, GlobalOffsetRep, SpatialEmbedding
 
 
 class TestTargetGenerator:
@@ -22,7 +18,7 @@ class TestTargetGenerator:
 
         class DummyTarget(target_generator.TargetGenerator):
             def __init__(self, xextent, yextent, img_shape):
-                super().__init__()
+                super().__init__(ix_low=0, ix_high=0)
                 self.xextent = xextent
                 self.yextent = yextent
                 self.img_shape = img_shape
@@ -46,7 +42,7 @@ class TestTargetGenerator:
 
     def test_shape(self, targ, fem):
         """
-        Tests the frame_shape
+        Tests the frame_shape of the target output
 
         Args:
             targ:
@@ -58,11 +54,11 @@ class TestTargetGenerator:
 
         """Tests"""
         assert out.dim() == 4, "Wrong dimensionality."
+        assert out.size()[-2:] == torch.Size(targ.img_shape), "Wrong output shape."
 
     @pytest.mark.parametrize("ix_low,ix_high", [(0, 0), (-1, 1)])
     @pytest.mark.parametrize("em_data", [EmptyEmitterSet(xy_unit='px'), RandomEmitterSet(10, xy_unit='px')])
     def test_default_range(self, targ, ix_low, ix_high, em_data):
-
         targ.ix_low = ix_low
         targ.ix_high = ix_high
 
@@ -73,189 +69,10 @@ class TestTargetGenerator:
         assert out.size(0) == ix_high - ix_low + 1
 
 
-class TestSpatialEmbedding:
-
-    @pytest.fixture(scope='class')
-    def tar_bin_1px(self):
-        return SpatialEmbedding((-0.5, 31.5),
-                                (-0.5, 31.5),
-                                (32, 32))
-
-    @pytest.fixture(scope='class')
-    def tar_bin_05px(self):
-        return SpatialEmbedding((-0.5, 31.5),
-                                (-0.5, 31.5),
-                                (64, 64))
-
-    @pytest.fixture(scope='class')
-    def delta_1px(self):
-        return psf_kernel.DeltaPSF((-0.5, 31.5), (-0.5, 31.5), (32, 32))
-
-    @pytest.fixture(scope='class')
-    def delta_05px(self):
-        return psf_kernel.DeltaPSF((-0.5, 31.5), (-0.5, 31.5), (64, 64))
-
-    def test_binning(self, tar_bin_1px, tar_bin_05px):
-        """
-        Tests the bins
-
-        Returns:
-
-        """
-        assert -0.5 == tar_bin_1px._bin_x[0]
-        assert 0.5 == tar_bin_1px._bin_x[1]
-        assert 0. == tar_bin_1px._bin_ctr_x[0]
-        assert 0. == tar_bin_1px._bin_ctr_y[0]
-
-        assert -0.5 == tar_bin_05px._bin_x[0]
-        assert 0. == tar_bin_05px._bin_x[1]
-        assert -0.25 == tar_bin_05px._bin_ctr_x[0]
-        assert -0.25 == tar_bin_05px._bin_ctr_y[0]
-
-        assert 0.5 == tar_bin_1px._offset_max_x
-        assert 0.5 == tar_bin_1px._offset_max_x
-        assert 0.25 == tar_bin_05px._offset_max_y
-        assert 0.25 == tar_bin_05px._offset_max_y
-
-    def test_forward_range(self, tar_bin_05px, tar_bin_1px):
-        em = CoordinateOnlyEmitter(torch.rand((1000, 3)) * 40, xy_unit='px')
-        offset_1px = tar_bin_1px.forward(em)
-        offset_hpx = tar_bin_05px.forward(em)
-
-        assert offset_1px.max().item() <= 0.5
-        assert offset_1px.min().item() > -0.5
-        assert offset_hpx.max().item() <= 0.25
-        assert offset_hpx.min().item() > -0.25
-
-    def test_forward_indexing_hc(self, tar_bin_1px, tar_bin_05px, delta_1px, delta_05px):
-        """
-        Test whether delta psf and offset map share the same indexing
-        """
-        xyz = CoordinateOnlyEmitter(torch.tensor([[15.1, 2.9, 0.]]), xy_unit='px').xyz
-
-        img_ = delta_1px.forward(xyz)
-        tar_ = tar_bin_1px.forward_(xyz, torch.zeros_like(xyz[:, 0]).long())
-        assert torch.equal(img_.nonzero(), tar_[:, 0].nonzero())
-        assert torch.equal(img_.nonzero(), tar_[:, 1].nonzero())
-
-        img_ = delta_05px.forward(xyz)
-        tar_ = tar_bin_05px.forward_(xyz, torch.zeros_like(xyz[:, 0]).long())
-        assert torch.equal(img_.nonzero(), tar_[:, 0].nonzero())
-        assert torch.equal(img_.nonzero(), tar_[:, 1].nonzero())
-
-        """Test an out of range emitter."""
-        xyz = CoordinateOnlyEmitter(torch.tensor([[31.6, 16., 0.]]), xy_unit='px').xyz
-        offset_map = tar_bin_1px.forward_(xyz, torch.zeros_like(xyz[:, 0]).long())
-        assert torch.equal(torch.zeros_like(offset_map), offset_map)
-
-    def test_forward_offset_1px_units(self, tar_bin_1px, tar_bin_05px):
-        """
-        Another value test during implementation
-
-        Args:
-            tar_bin_1px: fixture
-            tar_bin_05px: fixture
-
-        """
-        xyz = CoordinateOnlyEmitter(torch.tensor([[0., 0., 0.],
-                                                  [1.5, 1.2, 0.],
-                                                  [2.7, 0.5, 0.]])).xyz
-
-        offset_1px = tar_bin_1px.forward_(xyz, torch.zeros_like(xyz[:, 0]).long()).squeeze(0)
-
-        assert torch.allclose(torch.tensor([0., 0.]), offset_1px[:, 0, 0])
-        assert torch.allclose(torch.tensor([-0.5, 0.2]), offset_1px[:, 2, 1])
-        assert torch.allclose(torch.tensor([-0.3, -0.5]), offset_1px[:, 3, 1])
-
-
-class TestSinglePxEmbedding:
-
-    @pytest.fixture(scope='class')
-    def offset_rep(self):
-        return SinglePxEmbedding(xextent=(-0.5, 31.5), yextent=(-0.5, 31.5), img_shape=(32, 32))
-
-    @pytest.fixture(scope='class')
-    def em_set(self):
-        num_emitter = 100
-        return EmitterSet(torch.rand((num_emitter, 3)) * 40,
-                          torch.rand(num_emitter),
-                          torch.zeros(num_emitter).int(), xy_unit='px')
-
-    def test_forward_equal_nonzeroness(self, offset_rep, em_set):
-        """
-        Test whether the non-zero entries are the same in all channels. Assumes that nothing is exactly at 0.0...
-        """
-        out = offset_rep.forward_(em_set.xyz, em_set.phot, em_set.frame_ix)
-        assert equal_nonzero(out[:, 0], out[:, 1], out[:, 2], out[:, 3], out[:, 4])
-
-    def test_output_range(self, offset_rep, em_set):
-        """
-        Test whether delta x/y are between -0.5 and 0.5 (which they need to be for 1 coordinate unit == 1px)
-        """
-        out = offset_rep.forward_(em_set.xyz, em_set.phot, em_set.frame_ix)
-        p, I, dx, dy, z = out[:, 0], out[:, 1], out[:, 2], out[:, 3], out[:, 4]
-
-        assert (dx <= 0.5).all(), "delta x/y must be between -0.5 and 0.5"
-        assert (dx >= -0.5).all(), "delta x/y must be between -0.5 and 0.5"
-        assert (dy <= 0.5).all(), "delta x/y must be between -0.5 and 0.5"
-        assert (dy >= -0.5).all(), "delta x/y must be between -0.5 and 0.5"
-
-    def test_single_emitter(self, offset_rep):
-        """
-        Tests a single emitter for which I know the exact values.
-        Args:
-            offset_rep: fixture
-
-        """
-        em_set = CoordinateOnlyEmitter(torch.tensor([[15.1, 19.6, 250.]]), xy_unit='px')
-        out = offset_rep.forward_(em_set.xyz, em_set.phot, em_set.frame_ix)[0]  # single frame
-        assert tutil.tens_almeq(out[:, 15, 20], torch.tensor([1., 1., 0.1, -0.4, 250.]), 1e-5)
-
-
-class TestKernelEmbedding(TestSinglePxEmbedding):
-
-    @pytest.fixture(scope='class')
-    def roi_offset(self):
-        return KernelEmbedding((-0.5, 31.5), (-0.5, 31.5), (32, 32))
-
-    @pytest.fixture(scope='class')
-    def em_set_fixed_values(self):
-        """
-        Fixture to test hand-calculated values.
-        One easy emitter, two adjacent emitters, two overlaying emitters.
-        """
-        return CoordinateOnlyEmitter(torch.tensor([[14.9, 17.2, 300.],
-                                                   [0.01, 0.01, 250.],
-                                                   [0.99, 0.99, -250.],
-                                                   [25., 25., 500.],
-                                                   [25.2, 25.2, 700.],
-                                                   [10., 10., 200.],
-                                                   [11., 11., 500.]]), xy_unit='px')
-
-    def test_forward_equal_nonzeroness(self, roi_offset, em_set):
-        """
-        Test whether the non-zero entries are the same in all channels. Assumes that nothing is exactly at 0.0...
-        """
-        out = roi_offset.forward_(em_set.xyz, em_set.phot, em_set.frame_ix)
-        assert equal_nonzero(out[:, 1], out[:, 2], out[:, 3], out[:, 4])
-
-    def test_values(self, roi_offset, em_set_fixed_values):
-        target = roi_offset.forward_(em_set_fixed_values.xyz, em_set_fixed_values.phot, None)[0]  # single frame
-
-        assert tutil.tens_almeq(target[:, 15, 17], torch.tensor([1., 1., -0.1, 0.2, 300.]), 1e-5)
-        assert tutil.tens_almeq(target[2, 15, 16:19], torch.tensor([-0.1, -0.1, -0.1]), 1e-5)
-        assert tutil.tens_almeq(target[3, 14:17, 17], torch.tensor([0.2, 0.2, 0.2]), 1e-5)
-
-    def test_forward(self, roi_offset, em_set):
-
-        _ = roi_offset.forward(em_set)
-
-
 class TestUnifiedEmbeddingTarget(TestTargetGenerator):
 
     @pytest.fixture()
     def targ(self):
-
         xextent = (-0.5, 63.5)
         yextent = (-0.5, 63.5)
         img_shape = (64, 64)
@@ -284,7 +101,6 @@ class TestUnifiedEmbeddingTarget(TestTargetGenerator):
 
     @pytest.mark.parametrize("roi_size", torch.tensor([1, 3, 5, 7]))
     def test_roi_px(self, targ, random_emitter, roi_size):
-
         """Setup"""
         targ.__init__(xextent=targ.xextent, yextent=targ.yextent, img_shape=targ.img_shape,
                       roi_size=roi_size, ix_low=targ.ix_low, ix_high=targ.ix_high)
@@ -308,77 +124,13 @@ class TestUnifiedEmbeddingTarget(TestTargetGenerator):
         assert (off_y.unique() == expct_vals).all()
 
     def test_forward(self, targ):
+        """Test a couple of handcrafted cases"""
 
-        em_set = CoordinateOnlyEmitter(torch.tensor([[15.1, 19.6, 250.]]), xy_unit='px')
+        # one emitter outside fov the other one inside
+        em_set = CoordinateOnlyEmitter(torch.tensor([[-50., 0., 0.], [15.1, 19.6, 250.]]), xy_unit='px')
+        em_set.phot = torch.tensor([5., 4.])
+
         out = targ.forward(em_set)[0]  # single frame
-        assert tutil.tens_almeq(out[:, 15, 20], torch.tensor([1., 1., 0.1, -0.4, 250.]), 1e-5)
-        assert tutil.tens_almeq(out[:, 16, 20], torch.tensor([0., 1., -0.9, -0.4, 250.]), 1e-5)
-        assert tutil.tens_almeq(out[:, 15, 21], torch.tensor([0., 1., 0.1, -1.4, 250.]), 1e-5)
-
-
-@pytest.mark.skip("Deprecated.")
-class TestGlobalOffsetRep:
-
-    @pytest.fixture(scope='class')
-    def classyclassclass(self):
-        return GlobalOffsetRep((-0.5, 31.5), (-0.5, 31.5), None, (32, 32))
-
-    @pytest.fixture(scope='class')
-    def two_em(self):
-        return CoordinateOnlyEmitter(torch.tensor([[-0.5, 15.5, 0.], [31.5, 15.5, 0.]]))
-
-    def test_forward_shape(self, classyclassclass, two_em):
-        assert classyclassclass.assign_emitter(two_em).shape == torch.zeros(32, 32).shape
-
-    def test_classification(self, classyclassclass, two_em):
-        gt = torch.zeros(32, 32).type(torch.LongTensor)
-        gt[16:, :] = 1
-
-        prediction_ix = classyclassclass.assign_emitter(two_em)
-        assert tutil.tens_almeq(gt, prediction_ix)
-
-    @pytest.mark.skip("Only for plotting.")
-    def test_diag_em(self, classyclassclass):
-        em = CoordinateOnlyEmitter(torch.tensor([[0., 0., 0.], [31., 31., 0.]]))
-        prediction_ix = classyclassclass.assign_emitter(em)
-        prediction_ix = prediction_ix.reshape(classyclassclass.img_shape[0], classyclassclass.img_shape[1])
-
-        PlotFrame(prediction_ix).plot()
-        plt.show()
-
-        em = RandomEmitterSet(10, extent=32)
-        ix_map = classyclassclass.assign_emitter(em)
-        ix_map = ix_map.reshape(classyclassclass.img_shape[0], classyclassclass.img_shape[1])
-        PlotFrameCoord(frame=ix_map, pos_tar=em.xyz).plot()
-        plt.show()
-
-        offset_maps = classyclassclass.forward_(em)
-        PlotFrameCoord(frame=offset_maps[2], pos_tar=em.xyz).plot()
-        plt.show()
-
-        PlotFrameCoord(frame=offset_maps[3], pos_tar=em.xyz).plot()
-        plt.show()
-
-        PlotFrameCoord(frame=offset_maps[4], pos_tar=em.xyz).plot()
-        plt.show()
-        assert True
-
-    @pytest.mark.skip("Only for plotting.")
-    def test_mask(self, classyclassclass):
-        classyclassclass.masked = True
-
-        em = RandomEmitterSet(10, extent=32)
-        em.phot = torch.randn_like(em.phot)
-        offset_maps = classyclassclass.forward_(em)
-        PlotFrameCoord(frame=offset_maps[1], pos_tar=em.xyz).plot()
-        plt.show()
-
-        PlotFrameCoord(frame=offset_maps[2], pos_tar=em.xyz).plot()
-        plt.show()
-
-        PlotFrameCoord(frame=offset_maps[3], pos_tar=em.xyz).plot()
-        plt.show()
-
-        PlotFrameCoord(frame=offset_maps[4], pos_tar=em.xyz).plot()
-        plt.show()
-        assert True
+        assert tutil.tens_almeq(out[:, 15, 20], torch.tensor([1., 4., 0.1, -0.4, 250.]), 1e-5)
+        assert tutil.tens_almeq(out[:, 16, 20], torch.tensor([0., 4., -0.9, -0.4, 250.]), 1e-5)
+        assert tutil.tens_almeq(out[:, 15, 21], torch.tensor([0., 4., 0.1, -1.4, 250.]), 1e-5)
