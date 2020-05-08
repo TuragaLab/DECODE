fd493d9b4964dee19a50d6a8fafdb11100a17560
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2020-05-08 15:24:50 +0200

    functionalise emitter IO

--- deepsmlm/utils/emitter_io.py
+++ deepsmlm/utils/emitter_io.py
@@ -1,88 +1,42 @@
-from abc import ABC  # abstract class
+import pathlib
 
+import h5py
 import numpy as np
 import torch
 
-import deepsmlm
-
-"""
-Interfaces should provide the minimum set of variables that are obligatory to create an emitterset.
-"""
+from deepsmlm.generic import emitter
 
 
-class BinaryInterface(ABC):
-    """
-    Abstract class to specify binary interfaces
+def load_smap_emitter(file: (str, pathlib.Path), mapping: (dict, None) = None, **emitter_kwargs) -> emitter.EmitterSet:
     """
 
-    def __init__(self):
-        super().__init__()
+    Args:
+        file: .mat file
+        mapping (optional): mapping of matlab fields to emitter
+        **emitter_kwargs: additional arguments to be parsed to the emitter initialisation
 
-    def load(self):
-        raise NotImplementedError
+    Returns:
 
-    def save(self):
-        raise NotImplementedError
-
-
-class MatlabInterface(BinaryInterface):
     """
-    Load an SML localisation file from SMAP. Assumes it is stored in the modern -v7.3 format (i.e. a HDF5).
-    """
+    if mapping is None:
+        mapping = {'x': 'xnm', 'y': 'ynm', 'z': 'znm',
+                   'phot': 'phot', 'frame_ix': 'frame', 'bg': 'bg'}
 
-    x_key = 'xnm'
-    y_key = 'ynm'
-    z_key = 'znm'
-    phot_key = 'phot'
-    frame_key = 'frame'
-    bg_key = 'bg'
+    f = h5py.File(file, 'r')
 
-    xy_unit = 'nm'
+    loc_dict = f['saveloc']['loc']
+    xyz = torch.cat([
+        torch.from_numpy(np.array(loc_dict[mapping['x']])).permute(1, 0),
+        torch.from_numpy(np.array(loc_dict[mapping['y']])).permute(1, 0),
+        torch.from_numpy(np.array(loc_dict[mapping['z']])).permute(1, 0)
+    ], 1)
 
-    def __init__(self, frame_shift=-1, axis=[1, 0, 2]):
-        """
-        Specifies some transformations
+    em = emitter.EmitterSet(xyz=xyz,
+                            phot=torch.from_numpy(np.array(loc_dict[mapping['phot']])).squeeze(),
+                            frame_ix=torch.from_numpy(np.array(loc_dict[mapping['frame']])).squeeze(),
+                            bg=torch.from_numpy(np.array(loc_dict[mapping['bg']])).squeeze(),
+                            xy_unit='nm', **emitter_kwargs)
 
-        Args:
-            frame_shift: transform frame index. Usually -1 because MATLAB counts starting at 1, Python 0
-            axis: swap axis
+    em.frame_ix -= 1  # MATLAB starts at 1, python and all serious languages at 0
 
-        """
-        super().__init__()
-
-        self.frame_shift = frame_shift
-        self.axis_trafo = axis
-        self._cache_sml = None
-
-    def load(self, mat_file):
-        """
-        Loads the savelocs struct from Matlab. Applies transformations as specified
-
-        Args:
-            mat_file:
-
-        Returns:
-            (EmitterSet)
-        """
-        import h5py
-
-        f = h5py.File(mat_file, 'r')
-        self._cache_sml = f
-
-        loc_dict = f['saveloc']['loc']
-        xyz = torch.cat([
-            torch.from_numpy(np.array(loc_dict[self.x_key])).permute(1, 0),
-            torch.from_numpy(np.array(loc_dict[self.y_key])).permute(1, 0),
-            torch.from_numpy(np.array(loc_dict[self.z_key])).permute(1, 0)
-        ], 1)
-
-        em = deepsmlm.EmitterSet(xyz=xyz,
-                                 phot=torch.from_numpy(np.array(loc_dict[self.phot_key])).squeeze(),
-                                 frame_ix=torch.from_numpy(np.array(loc_dict[self.frame_key])).squeeze(),
-                                 bg=torch.from_numpy(np.array(loc_dict[self.bg_key])).squeeze(),
-                                 xy_unit=self.xy_unit)
-
-        em.convert_em_(axis=self.axis_trafo)
-        em.frame_ix += self.frame_shift
-
-        return em
+    return em
