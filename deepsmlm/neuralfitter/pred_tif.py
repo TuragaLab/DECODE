83253440ee73d5e16045d65cb81d3447b7845177
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2020-05-26 17:12:17 +0200

    Rm load tif because this is in utils now

--- deepsmlm/neuralfitter/pred_tif.py
+++ deepsmlm/neuralfitter/pred_tif.py
@@ -1,8 +1,6 @@
 import csv
-import pathlib
 from abc import ABC, abstractmethod  # abstract class
 
-import tifffile
 import torch
 import torch.utils
 from tqdm import tqdm
@@ -172,48 +170,6 @@ class PredictEvalTif(PredictEval):
         self.frames = None
         self.dataset = None
         self.dataloader = None
-
-    @staticmethod
-    def load_tif(file: (str, pathlib.Path)):
-        """
-        Reads the tif(f) files. When a folder is specified, potentially multiple files are loaded.
-        Which are stacked into a new first axis.
-        Make sure that if you provide multiple files (i.e. a folder) sorting gives the correct order. Otherwise we can
-        not guarantee anything.
-
-        Args:
-            no_init: (bool) do not init dataset. useful if you want to manipulate your data first
-
-        Returns:
-
-        """
-        p = pathlib.Path(file)
-
-        # if dir, load multiple files and stack them if more than one found
-        if p.is_dir():
-            print("Path to folder of tifs specified. Traversing through the directory.")
-
-            file_list = sorted(p.glob('*.tif*'))  # load .tif or .tiff
-            frames = []
-            for f in tqdm(file_list):
-                frames.append(torch.from_numpy(tifffile.imread(str(f)).astype('float32')))
-
-            print("Tiffs successfully read.")
-            if frames.__len__() >= 2:
-                print("Multiple tiffs found. Stacking them ...")
-                frames = torch.stack(frames, 0)
-            else:
-                frames = frames[0]
-
-        else:
-            im = tifffile.imread(str(p))
-            print("Tiff successfully read.")
-            frames = torch.from_numpy(im.astype('float32'))
-
-        if frames.squeeze().ndim <= 2:
-            raise ValueError("Tif seems to be of wrong dimension or could only find a single frame.")
-
-        return frames
 
     @staticmethod
     def load_csv(activation_file, verbose=False):
