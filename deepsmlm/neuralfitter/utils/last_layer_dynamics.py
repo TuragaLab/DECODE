72956afcb593dcfb68841bb153f3fce8d2a2c739
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2020-06-04 11:59:44 +0200

    Clearify weight by gradient function

--- deepsmlm/neuralfitter/utils/last_layer_dynamics.py
+++ deepsmlm/neuralfitter/utils/last_layer_dynamics.py
@@ -1,28 +1,43 @@
+from typing import Tuple
+
 import torch
 
 
-def rescale_last_layer_grad(heads, loss, optimizer):
+def weight_by_gradient(layer: torch.nn.ModuleList, loss: torch.Tensor, optimizer: torch.optim.Optimizer) -> Tuple[
+    torch.Tensor, torch.Tensor, torch.Tensor]:
     """
 
-    :param loss: non-reduced loss of size N x C x H x W
-    :param optimizer:
-    :return: weight, channelwise loss, channelwise weighted loss
+    Args:
+        layer: module layers
+        loss: not reduced loss values
+        optimizer: optimizer
+
+    Returns:
+        weight_cX_h1_w1: weight per channel (1x C x 1 x 1)
+        loss_ch: channel-wise loss
+        loss_w: weighted loss
+
     """
+
     """
     Reduce NCHW channel wise. Division over numel and multiply by ch_out is not needed inside this method, but if
     you want to use loss_wch, or loss_ch directly the numbers would be off by a factor
     """
-    ch_out = heads.__len__()
+    ch_out = len(layer)
     loss_ch = loss.sum(-1).sum(-1).sum(0) / loss.numel() * ch_out
     head_grads = torch.zeros((ch_out,)).to(loss.device)
     weighting = torch.ones_like(head_grads).to(loss.device)
 
     for i in range(ch_out):
-        head_grads[i] = torch.autograd.grad(loss_ch[i], heads[i].out_conv.weight, retain_graph=True)[0].abs().sum()
+        head_grads[i] = torch.autograd.grad(loss_ch[i], layer[i].out_conv.weight, retain_graph=True)[0].abs().sum()
+
+    """Kill the channels which are completely inactive"""
+    ix_on = head_grads != 0.
+    weighting[~ix_on] = 0.  # set excluded to zero
 
     optimizer.zero_grad()
-    N = (1 / head_grads).sum()
-    weighting = weighting / head_grads
+    N = (1 / head_grads[ix_on]).sum()
+    weighting[ix_on] = weighting[ix_on] / head_grads[ix_on]
     weighting = weighting / N
 
     loss_wch = (loss_ch * weighting).sum()
