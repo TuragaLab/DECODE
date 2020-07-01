48cbe54605adeabf6ff05f001ab2072980ca5ba1
Lucas-Rapahel Müller <gitdev@LRM.photo>
2020-07-01 17:25:46 +0200

    Simplify emitter comparison

--- deepsmlm/generic/test_utils.py
+++ deepsmlm/generic/test_utils.py
@@ -1,16 +1,16 @@
 import torch
 
 
-def tens_almeq(a: torch.Tensor, b: torch.Tensor, prec: float = 1e-8, nan: bool = False):
+def tens_almeq(a: torch.Tensor, b: torch.Tensor, prec: float = 1e-8, nan: bool = False) -> bool:
     """
     Tests if a and b are equal (i.e. all elements are the same) within a given precision. If both tensors have / are
     nan, the function will return False unless nan=True.
 
     Args:
-        a (torch.Tensor):
-        b (torch.Tensor):
-        prec (float):
-        nan (bool): if true, the function will return true if both tensors are all nan
+        a: first tensor for comparison
+        b: second tensor for comparison
+        prec: precision comparison
+        nan: if true, the function will return true if both tensors are all nan
 
     Returns:
         bool
@@ -27,4 +27,4 @@ def tens_almeq(a: torch.Tensor, b: torch.Tensor, prec: float = 1e-8, nan: bool =
         if torch.isnan(a).all() and torch.isnan(b).all():
             return True
 
-    return torch.all(torch.lt(torch.abs(torch.add(a, -b)), prec))
+    return torch.all(torch.lt(torch.abs(torch.add(a, -b)), prec)).item()
