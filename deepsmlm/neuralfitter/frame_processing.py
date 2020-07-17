5826e7602e11dfed773e57760d48a6a768057efc
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2020-07-17 21:35:55 +0200

    AutoCenterCrop

--- deepsmlm/neuralfitter/frame_processing.py
+++ deepsmlm/neuralfitter/frame_processing.py
@@ -35,3 +35,36 @@ class Mirror2D(FrameProcessing):
 
     def forward(self, frame: torch.Tensor) -> torch.Tensor:
         return frame.flip(self.dims)
+
+
+class AutoCenterCrop(FrameProcessing):
+
+    def __init__(self, px_fold: int):
+        """
+        Automatic cropping in centre. Specify pixel_fold which the target frame size must satistfy
+        and the frame will be center-cropped to this size.
+
+        Args:
+            px_fold: integer in which multiple the frame must dimensioned (H, W dimension)
+
+        """
+        super().__init__()
+        self.px_fold = px_fold
+
+        if not isinstance(self.px_fold, int):
+            raise ValueError
+
+    def forward(self, frame: torch.Tensor) -> torch.Tensor:
+
+        # actual size
+        size_is = torch.tensor(frame.size())[-2:]
+        size_tar = (torch.tensor(size_is) / self.px_fold) * self.px_fold  # integer divison
+
+        if (size_tar <= 0).any():
+            raise ValueError
+
+        """Crop"""
+        ix_front = ((size_is - size_tar).float() / 2).ceil().long()
+        ix_back = ix_front + size_tar
+
+        return frame[..., ix_front[0]:ix_back[0], ix_front[1]:ix_back[1]]
--- deepsmlm/test/test_frame_processing.py
+++ deepsmlm/test/test_frame_processing.py
@@ -22,3 +22,29 @@ class TestMirror:
 
         """Assert"""
         assert test_utils.tens_almeq(x_out, x_tar)
+
+
+class TestAutoCenterCrop:
+
+    @pytest.fixture()
+    def proc(self):
+        return frame_processing.AutoCenterCrop(4)
+
+    @pytest.mark.parametrize("actual_size,target_size", [((5, 6), (4, 4)),
+                                                         ((5, 8), (4, 8)),
+                                                         ((12, 4), (12, 4)),
+                                                         ((13, 25), (12, 24)),
+                                                         ((3, 8), 'err')])
+    def test_forward(self, actual_size, target_size, proc):
+
+        """Setup"""
+        x = torch.rand((2, *actual_size))
+
+        """Forward"""
+        if target_size == 'err':
+            with pytest.raises(ValueError):
+                proc.forward(x)
+
+        else:
+            x_out = proc.forward(x)
+            assert x_out[0].size() == torch.Size(target_size)
