0f7970b74875bd19d3b47be1dffe992d9775f0aa
Lucas-Rapahel Müller <gitdev@LRM.photo>
2020-07-30 17:30:33 +0200

    Make static cuda check public static method. Purify

--- decode/simulation/psf_kernel.py
+++ decode/simulation/psf_kernel.py
@@ -418,8 +418,25 @@ class CubicSplinePSF(PSF):
                           "While this mostly likely works computationally, results may be unexpected.")
 
     @property
-    def cuda_is_available(self):
-        return self._spline_impl.cuda_is_available
+    def cuda_is_available(self) -> bool:
+        """
+        Returns true if (1) a CUDA capable device is available and (2) spline was compiled with CUDA support.
+        Technically (1) could come without (2).
+
+        """
+        if torch.cuda.is_available():
+            return self._spline_impl.cuda_is_available
+
+        return False
+
+    @staticmethod
+    def check_cuda_is_available() -> bool:
+        """
+        This is a dummy method to check whether CUDA is available without the need to init the class. I wonder
+        whether Python has 'static properties'?
+
+        """
+        return spline.PSFWrapperCPU(1, 1, 1, 1, 1, torch.zeros((1, 64)).numpy()).cuda_is_available
 
     @property
     def _ref_diff(self):
@@ -427,15 +444,6 @@ class CubicSplinePSF(PSF):
             return torch.zeros((3))
         else:
             return self.ref_re - self.ref0
-
-    @staticmethod
-    def _cuda_compiled():
-        """
-        This is a dummy method to check whether CUDA is available without the need to init the class.
-        Returns:
-
-        """
-        return spline.PSFWrapperCPU(1, 1, 1, 1, 1, torch.zeros((1, 64)).numpy()).cuda_is_available
 
     @property
     def _roi_size_nm(self):
