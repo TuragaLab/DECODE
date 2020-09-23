2ce6bb158b96f70b961bec335c9a6de4031fd891
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2020-09-23 14:00:55 -0400

    added runtime requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,40 @@ import setuptools
 
 from setuptools import setup
 
+# requirements when installing the wheel via pip install; conda install uses
+#   info in meta.yaml instead; we're supporting multiple environments, thus
+#   we have to accept some duplication (or near-duplication), unfortunately
+requirements = [
+    "numpy",
+    "dotmap",
+    "torch",
+    "torchvision",
+    "click",
+    "deprecated",
+    "dotmap",
+    "h5py",
+    "joblib",
+    "matplotlib",
+    "pandas",
+    "pytest",
+    "pyyaml",
+    "requests",
+    "scipy",
+    "seaborn",
+    "scikit-image",
+    "scikit-learn",
+    "tensorboard",
+    "tifffile",
+    "tqdm",
+    ]
+
+
 setup(
     name='decode',
     version='0.1.dev0',
     packages=setuptools.find_packages(),
     include_package_data=True,
+    install_requires=requirements,
     zip_safe=False,
     url='https://rieslab.de',
     license='',
