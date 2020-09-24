b2f93645d9ed2f850ef063312f1d9700be219317
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2020-09-24 14:41:38 -0400

    reset version for more development

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requirements = [
 
 setup(
     name='decode',
-    version='0.9',
+    version='0.9.1dev',
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=requirements,
