390a07a2b41cee12eefffdb0e7345bd905b4f2cc
Lucas-Rapahel Müller <gitdev@LRM.photo>
2020-09-30 22:26:37 +0200

    Added version and author to decode init

--- decode/__init__.py
+++ decode/__init__.py
@@ -1,3 +1,12 @@
+"""
+DECODE
+This software package implements a DeepLearning based framework for high-density fitting in SMLM.
+
+"""
+
+__version__ = '0.9.a'
+__author__ = 'Lucas-Raphael Müller, Artur Speiser'
+
 import decode.generic
 import decode.evaluation
 import decode.neuralfitter
