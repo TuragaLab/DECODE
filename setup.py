a30e553f16e8d6e6a86b79eb39296afb2118e28a
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2020-10-22 21:56:34 +0200

    Bump version: 0.9.3-beta.1 → 0.9.3-beta.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ else:
 
 setup(
     name='decode',
-    version='0.9.3-beta.1',
+    version='0.9.3-beta.2',
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=requirements,
