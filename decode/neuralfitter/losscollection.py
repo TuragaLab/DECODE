a4d3554f33a77138d21cc0eb89831f1efdf5ae36
Lucas Mueller <lucas.mueller@embl.de>
2021-01-17 19:29:30 +0100

    Rm deprecated loss

--- decode/neuralfitter/losscollection.py
+++ decode/neuralfitter/losscollection.py
@@ -131,47 +131,6 @@ class PPXYZBLoss(Loss):
         return tot_loss
 
 
-class FourFoldPPXYZ(Loss):
-    """
-    Four-fold implementation of PPXYZ Loss, i.e. half pixel shifted pointers.
-
-    """
-
-    def __init__(self, components):
-        """
-
-        Args:
-            components: respective PXYZ Loss instances
-
-        """
-        super().__init__()
-
-        self.com = components
-        self.bg_loss = torch.nn.MSELoss(reduction='none')
-
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
-
-    def log(self, loss_val):
-        return loss_val.mean().item(), {}
-
-    def _forward_checks(output, target, weight):
-        pass
-
-    def forward(self, output, target, weight):
-        assert output.size(1) % len(self.com) == 1
-        n_ch_per_com = (output.size(1) - 1) // len(self.com)
-
-        loss = torch.cat(
-            [self.com[i].forward(output[:, c:c + n_ch_per_com],
-                                 target[:, c:c + n_ch_per_com],
-                                 weight[:, c:c + n_ch_per_com]) for i, c in enumerate(range(0, 20, 5))], 1)
-
-        loss = torch.cat((loss, self.bg_loss(output[:, [-1]], target[:, [-1]]) * weight[:, [-1]]), 1)
-
-        return loss
-
-
 class GaussianMMLoss(Loss):
     """
     Model output is a mean and sigma value which forms a gaussian mixture model.
