e04e34b1cb61d9a1d5821e934337c2da1d5ff31d
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2021-01-18 19:33:43 +0100

    Update (unit) test for train and test implementation

--- decode/test/test_train_val_impl.py
+++ decode/test/test_train_val_impl.py
@@ -1,13 +1,11 @@
 import pytest
 import torch
 
-from decode.generic import emitter
+from decode.neuralfitter import losscollection
+from decode.neuralfitter import post_processing
 from decode.neuralfitter import train_val_impl
 from decode.neuralfitter.models import unet_param
-from decode.neuralfitter import losscollection
 from decode.neuralfitter.utils import logger as logger_utils
-from decode.neuralfitter import post_processing
-from decode.neuralfitter.utils.collate import smlm_collate
 
 
 class TestTrain:
@@ -39,55 +37,34 @@ class TestTrain:
     def logger(self):
         return logger_utils.NoLog()
 
-    def test_train(self, model, opt, loss, dataloader, logger):
-        """Test the training function"""
-
-        """Setup / Checks"""
+    @pytest.fixture()
+    def train_val_environment(self, loss, model):
         device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
         # ship the things to the apropriate device
         loss.__init__(device=device)
         model = model.to(device)
         param_before = model.encoder[0][0].weight.data.clone()
+        return device, model, param_before
+
+    def test_iterate_batch(self, opt, loss, dataloader, logger, train_val_environment):
+        device, model, param_before = train_val_environment
 
         """Run"""
-        assert train_val_impl.train(model, opt, loss, dataloader, False, 0, device, logger) is None
+        train_val_impl.train(model, opt, loss, dataloader, False, False, 0, device, logger) is None
         param_after = model.encoder[0][0].weight.data.clone()
 
-        """Assert"""
         assert (param_before != param_after).any(), "No weights changed although they should have."
 
 
 class TestVal(TestTrain):
 
     @pytest.fixture()
-    def dataloader(self):
-        class MockDataset(torch.utils.data.Dataset):
-            def __len__(self):
-                return 256
-
-            def __getitem__(self, item):
-                return torch.rand((1, 64, 64)), \
-                       torch.rand((6, 64, 64)), \
-                       torch.rand((6, 64, 64)), \
-                       emitter.RandomEmitterSet(32)
-
-        return torch.utils.data.DataLoader(MockDataset(), batch_size=32, collate_fn=smlm_collate)
-
-    @pytest.fixture()
     def post_processor(self):
         return post_processing.ConsistencyPostprocessing(raw_th=0.1, em_th=0.5, xy_unit='nm', img_shape=(64, 64),
                                                          lat_th=100)
 
-    def test_train(self):
-        return
-
-    def test_val(self, model, opt, loss, dataloader, post_processor, logger):
-
-        """Setup / Checks"""
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        loss.__init__(device=device)
-        model = model.to(device)
-        param_before = model.encoder[0][0].weight.data.clone()
+    def test_iterate_batch(self, model, opt, loss, dataloader, post_processor, logger, train_val_environment):
+        device, model, param_before = train_val_environment
 
         """Run"""
         train_val_impl.test(model, loss, dataloader, 0, device)
@@ -95,4 +72,3 @@ class TestVal(TestTrain):
 
         """Assert"""
         assert (param_before == param_after).all(), "Weights must not change."
-
