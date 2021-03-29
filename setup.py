37cba199803176efa397d35cbee3bbc9b1f4312c
Lucas-Raphael Müller <lucas-raphael.mueller@dkfz-heidelberg.de>
2021-03-29 20:39:18 +0200

    Bump version: 0.10.0 → 0.10.1dev1

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ else:
 
 setup(
     name='decode',
-    version='0.10.0',  # do not modify by hand set and sync with bumpversion
+    version='0.10.1dev1',  # do not modify by hand set and sync with bumpversion
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=requirements,
