b531b2271a2d9ded5295f19f945bc1eec0c498d7
Lucas-Raphael Müller <lucas-raphael.mueller@dkfz-heidelberg.de>
2021-05-20 21:48:08 +0200

    Style

--- decode/neuralfitter/models/model_speced_impl.py
+++ decode/neuralfitter/models/model_speced_impl.py
@@ -1,7 +1,6 @@
-from typing import Union
-
 import torch
 from torch import nn
+from typing import Union
 
 from . import model_param
 
@@ -41,15 +40,18 @@ class SigmaMUNet(model_param.DoubleMUnet):
         )
 
         """Register sigma as parameter such that it is stored in the models state dict and loaded correctly."""
-        self.register_parameter('sigma_eps', torch.nn.Parameter(torch.tensor([self.sigma_eps_default]),
-                                                                requires_grad=False))
+        self.register_parameter('sigma_eps',
+                                torch.nn.Parameter(torch.tensor([self.sigma_eps_default]),
+                                                   requires_grad=False))
 
         if kaiming_normal:
             self.apply(self.weight_init)
 
             # custom
-            torch.nn.init.kaiming_normal_(self.mt_heads[0].core[0].weight, mode='fan_in', nonlinearity='relu')
-            torch.nn.init.kaiming_normal_(self.mt_heads[0].out_conv.weight, mode='fan_in', nonlinearity='linear')
+            torch.nn.init.kaiming_normal_(self.mt_heads[0].core[0].weight, mode='fan_in',
+                                          nonlinearity='relu')
+            torch.nn.init.kaiming_normal_(self.mt_heads[0].out_conv.weight, mode='fan_in',
+                                          nonlinearity='linear')
             torch.nn.init.constant_(self.mt_heads[0].out_conv.bias, -6.)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -68,14 +70,14 @@ class SigmaMUNet(model_param.DoubleMUnet):
 
         """Add epsilon to sigmas and rescale"""
         x[:, self.pxyz_sig_ch_ix] = x[:, self.pxyz_sig_ch_ix] * 3 + self.sigma_eps
-        
+
         """Disabled attributes get set to constants"""
         if self.disabled_attr_ix is not None:
             for ix in self.disabled_attr_ix:
                 # Set means to 0
-                x[:, 1+ix] = x[:, 1+ix]*0
+                x[:, 1 + ix] = x[:, 1 + ix] * 0
                 # Set sigmas to 0.1
-                x[:, 5+ix] = x[:, 5+ix]*0 + 0.1
+                x[:, 5 + ix] = x[:, 5 + ix] * 0 + 0.1
 
         return x
 
