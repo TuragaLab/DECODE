83f6eaf4f8ec695f683ad420cf59be3daf057123
Lucas-Raphael Mueller <git@lrm.dev>
2022-05-10 17:49:46 +0200

    clean up camera.py

--- decode/simulation/camera.py
+++ decode/simulation/camera.py
@@ -9,13 +9,16 @@ from ..neuralfitter import sampling
 
 
 class Camera(ABC):
-
     @abstractmethod
-    def forward(self, x: torch.Tensor, device: Union[str, torch.device] = None) -> torch.Tensor:
+    def forward(
+        self, x: torch.Tensor, device: Union[str, torch.device] = None
+    ) -> torch.Tensor:
         raise NotImplementedError
 
     @abstractmethod
-    def backward(self, x: torch.Tensor, device: Union[str, torch.device] = None) -> torch.Tensor:
+    def backward(
+        self, x: torch.Tensor, device: Union[str, torch.device] = None
+    ) -> torch.Tensor:
         raise NotImplementedError
 
 
@@ -26,8 +29,18 @@ class Photon2Camera(Camera):
 
     """
 
-    def __init__(self, *, qe: float, spur_noise: float, em_gain: Union[float, None], e_per_adu: float, baseline: float,
-                 read_sigma: float, photon_units: bool, device: Union[str, torch.device] = None):
+    def __init__(
+        self,
+        *,
+        qe: float,
+        spur_noise: float,
+        em_gain: Union[float, None],
+        e_per_adu: float,
+        baseline: float,
+        read_sigma: float,
+        photon_units: bool,
+        device: Union[str, torch.device] = None,
+    ):
         """
 
         Args:
@@ -57,19 +70,28 @@ class Photon2Camera(Camera):
     @classmethod
     def parse(cls, param):
 
-        return cls(qe=param.Camera.qe, spur_noise=param.Camera.spur_noise,
-                   em_gain=param.Camera.em_gain, e_per_adu=param.Camera.e_per_adu,
-                   baseline=param.Camera.baseline, read_sigma=param.Camera.read_sigma,
-                   photon_units=param.Camera.convert2photons,
-                   device=param.Hardware.device_simulation)
+        return cls(
+            qe=param.Camera.qe,
+            spur_noise=param.Camera.spur_noise,
+            em_gain=param.Camera.em_gain,
+            e_per_adu=param.Camera.e_per_adu,
+            baseline=param.Camera.baseline,
+            read_sigma=param.Camera.read_sigma,
+            photon_units=param.Camera.convert2photons,
+            device=param.Hardware.device_simulation,
+        )
 
     def __str__(self):
-        return f"Photon to Camera Converter.\n" + \
-               f"Camera: QE {self.qe} | Spur noise {self.spur} | EM Gain {self._em_gain} | " + \
-               f"e_per_adu {self.e_per_adu} | Baseline {self.baseline} | Readnoise {self._read_sigma}\n" + \
-               f"Output in Photon units: {self.photon_units}"
+        return (
+            f"Photon to Camera Converter.\n"
+            + f"Camera: QE {self.qe} | Spur noise {self.spur} | EM Gain {self._em_gain} | "
+            + f"e_per_adu {self.e_per_adu} | Baseline {self.baseline} | Readnoise {self._read_sigma}\n"
+            + f"Output in Photon units: {self.photon_units}"
+        )
 
-    def forward(self, x: torch.Tensor, device: Union[str, torch.device] = None) -> torch.Tensor:
+    def forward(
+        self, x: torch.Tensor, device: Union[str, torch.device] = None
+    ) -> torch.Tensor:
         """
         Forwards frame through camera
 
@@ -85,43 +107,36 @@ class Photon2Camera(Camera):
         elif self.device is not None:
             x = x.to(self.device)
 
-        """Clamp input to 0."""
-        x = torch.clamp(x, 0.)
+        # clamp input to 0
+        x = torch.clamp(x, 0.0)
 
-        """Poisson for photon characteristics of emitter (plus autofluorescence etc."""
+        # Poisson for photon characteristics of emitter (plus autofluorescence etc
         camera = self.poisson.forward(x * self.qe + self.spur)
 
-        """Gamma for EM-Gain (EM-CCD cameras, not sCMOS)"""
+        # Gamma for EM-Gain (EM-CCD cameras, not sCMOS)
         if self._em_gain is not None:
             camera = self.gain.forward(camera)
 
-        """Gaussian for read-noise. Takes camera and adds zero centred gaussian noise."""
+        # Gaussian for read-noise. Takes camera and adds zero centred gaussian noise
         camera = self.read.forward(camera)
 
-        """Electrons per ADU, (floor function)"""
+        # Electrons per ADU, (floor function)
         camera /= self.e_per_adu
         camera = camera.floor()
 
-        """Add Manufacturer baseline. Make sure it's not below 0."""
+        # Add Manufacturer baseline. Make sure it's not below 0
         camera += self.baseline
-        camera = torch.max(camera, torch.tensor([0.]).to(camera.device))
+        camera = torch.max(camera, torch.tensor([0.0]).to(camera.device))
 
         if self.photon_units:
             return self.backward(camera, device)
 
         return camera
 
-    def backward(self, x: torch.Tensor, device: Union[str, torch.device] = None) -> torch.Tensor:
-        """
-        Calculates the expected number of photons from a noisy image.
-
-        Args:
-            x:
-            device:
-
-        Returns:
-
-        """
+    def backward(
+        self, x: torch.Tensor, device: Union[str, torch.device] = None
+    ) -> torch.Tensor:
+        """Calculates the expected number of photons from a noisy image."""
 
         if device is not None:
             x = x.to(device)
@@ -141,31 +156,55 @@ class PerfectCamera(Photon2Camera):
     def __init__(self, device: Union[str, torch.device] = None):
         """
         Convenience wrapper for perfect camera, i.e. only shot noise. By design in 'photon units'.
-
-        Args:
-            device: device for simulation
-
         """
-        super().__init__(qe=1.0, spur_noise=0., em_gain=None, e_per_adu=1., baseline=0., read_sigma=0.,
-                         photon_units=False, device=device)
+        super().__init__(
+            qe=1.0,
+            spur_noise=0.0,
+            em_gain=None,
+            e_per_adu=1.0,
+            baseline=0.0,
+            read_sigma=0.0,
+            photon_units=False,
+            device=device,
+        )
 
     @classmethod
     def parse(cls, param):
         return cls(device=param.Hardware.device_simulation)
 
 
-@deprecated(reason="Not yet ready implementation. Needs thorough testing and validation.")
+@deprecated(
+    reason="Not yet ready implementation. Needs thorough testing and validation."
+)
 class SCMOS(Photon2Camera):
     """
     Models a sCMOS camera. You need provide a pixel-wise sigma map of the readout noise of the camera.
 
     """
 
-    def __init__(self, qe: float, spur_noise: float, em_gain: float, e_per_adu: float, baseline: float,
-                 read_sigma: torch.Tensor, photon_units: bool, sample_mode: str, device: (str, torch.device) = None):
+    def __init__(
+        self,
+        qe: float,
+        spur_noise: float,
+        em_gain: float,
+        e_per_adu: float,
+        baseline: float,
+        read_sigma: torch.Tensor,
+        photon_units: bool,
+        sample_mode: str,
+        device: (str, torch.device) = None,
+    ):
 
-        super().__init__(qe=qe, spur_noise=spur_noise, em_gain=em_gain, e_per_adu=e_per_adu,
-                         baseline=baseline, read_sigma=read_sigma, photon_units=photon_units, device=device)
+        super().__init__(
+            qe=qe,
+            spur_noise=spur_noise,
+            em_gain=em_gain,
+            e_per_adu=e_per_adu,
+            baseline=baseline,
+            read_sigma=read_sigma,
+            photon_units=photon_units,
+            device=device,
+        )
 
         self.sample_mode = sample_mode
 
@@ -174,7 +213,7 @@ class SCMOS(Photon2Camera):
         if self._read_sigma.dim() != 2:
             raise ValueError(f"Expected readout noise map to be 2D")
 
-        if self.sample_mode not in ('batch', 'const'):
+        if self.sample_mode not in ("batch", "const"):
             raise ValueError(f"Sample mode: {self.sample_mode} not supported.")
 
     def sample_sensor_window(self, size_nxy: tuple) -> torch.Tensor:
@@ -191,8 +230,9 @@ class SCMOS(Photon2Camera):
 
         return sampling.sample_crop(self._read_sigma, size_nxy)
 
-    def forward_on_sampled_sensor_window(self, x: torch.Tensor, device: Union[str, torch.device] = None) \
-            -> (torch.Tensor, torch.Tensor):
+    def forward_on_sampled_sensor_window(
+        self, x: torch.Tensor, device: Union[str, torch.device] = None
+    ) -> (torch.Tensor, torch.Tensor):
         """
         Forwards model input image 'x' through camera where x is possibly smaller than the camera sensor.
         A random window on the sensor is sampled and returned as second return argument.
@@ -207,9 +247,9 @@ class SCMOS(Photon2Camera):
         """
         if x.size() != self._read_sigma.size():
 
-            if self.sample_mode == 'const':
+            if self.sample_mode == "const":
                 sigma = self.sample_sensor_window((1, x.size(-2), x.size(-1)))
-            elif self.sample_mode == 'batch':
+            elif self.sample_mode == "batch":
                 sigma = self.sample_sensor_window((x.size(0), x.size(-2), x.size(-1)))
 
             if x.dim() == 4:
@@ -219,7 +259,9 @@ class SCMOS(Photon2Camera):
 
         return super().forward(x, device=device), sigma
 
-    def forward(self, x: torch.Tensor, device: Union[str, torch.device] = None) -> torch.Tensor:
+    def forward(
+        self, x: torch.Tensor, device: Union[str, torch.device] = None
+    ) -> torch.Tensor:
         """
         Forwards model input image 'x' through camera.
 
@@ -232,8 +274,10 @@ class SCMOS(Photon2Camera):
         """
 
         if x.size()[-2:] != self._read_sigma.size()[-2:]:
-            raise ValueError(f"Size of input does not match size of camera sensor. "
-                             f"Refer to method 'forward_on_sampled_sensor_window'")
+            raise ValueError(
+                f"Size of input does not match size of camera sensor. "
+                f"Refer to method 'forward_on_sampled_sensor_window'"
+            )
 
         else:
             self.read = noise_distributions.Gaussian(self._read_sigma.to(device))
--- test/unit/simulation/test_photcamera.py
+++ test/unit/simulation/test_photcamera.py
@@ -6,11 +6,17 @@ from decode.generic import test_utils
 
 
 class TestPhoton2Camera:
-
-    @pytest.fixture(scope='class')
+    @pytest.fixture(scope="class")
     def cam_fix(self):
-        return camera.Photon2Camera(qe=1.0, spur_noise=0.002, em_gain=300., e_per_adu=45.,
-                                    baseline=100, read_sigma=74.4, photon_units=False)
+        return camera.Photon2Camera(
+            qe=1.0,
+            spur_noise=0.002,
+            em_gain=300.0,
+            e_per_adu=45.0,
+            baseline=100,
+            read_sigma=74.4,
+            photon_units=False,
+        )
 
     def test_shape(self, cam_fix):
         x = torch.rand((32, 3, 64, 64))
@@ -29,21 +35,31 @@ class TestPhoton2Camera:
         tol = 0.01
         assert abs((x.mean() - out.mean()) / x.mean()) <= tol
 
-    @pytest.mark.skipif(not torch.cuda.is_available(), reason="Shipping to CUDA makes only sense if CUDA is available.")
+    @pytest.mark.skipif(
+        not torch.cuda.is_available(),
+        reason="Shipping to CUDA makes only sense if CUDA is available.",
+    )
     @pytest.mark.parametrize("input_device", ["cpu", "cuda"])
     @pytest.mark.parametrize("forward_device", ["cpu", "cuda"])
     def test_forward_backward_device(self, cam_fix, input_device, forward_device):
         exp_device = torch.rand(1).to(forward_device).device
 
-        assert cam_fix.forward(torch.rand((1, 32, 32)).to(input_device), forward_device).device \
-               == exp_device
+        assert (
+            cam_fix.forward(
+                torch.rand((1, 32, 32)).to(input_device), forward_device
+            ).device
+            == exp_device
+        )
 
-        assert cam_fix.backward(torch.rand((1, 32, 32)).to(input_device), forward_device).device \
-               == exp_device
+        assert (
+            cam_fix.backward(
+                torch.rand((1, 32, 32)).to(input_device), forward_device
+            ).device
+            == exp_device
+        )
 
 
 class TestPerfectCamera(TestPhoton2Camera):
-
     @pytest.fixture()
     def cam_fix(self):
         return camera.PerfectCamera()
@@ -59,10 +75,17 @@ class TestPerfectCamera(TestPhoton2Camera):
 
 @pytest.mark.skip(reason="Implementation not ready.")
 class TestSCMOS(TestPhoton2Camera):
-
     @pytest.fixture()
     def cam_fix(self):
         read_sigma = torch.meshgrid(torch.arange(256), torch.arange(256))[0]
 
-        return camera.SCMOS(sample_mode='batch', qe=1.0, spur_noise=0.002, em_gain=300., e_per_adu=45.,
-                            baseline=100, read_sigma=read_sigma, photon_units=False)
+        return camera.SCMOS(
+            sample_mode="batch",
+            qe=1.0,
+            spur_noise=0.002,
+            em_gain=300.0,
+            e_per_adu=45.0,
+            baseline=100,
+            read_sigma=read_sigma,
+            photon_units=False,
+        )
