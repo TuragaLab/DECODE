f44e5665333846bc08afba4dc654fea9e9dffe42
Lucas-Raphael Mueller <git@lrm.dev>
2022-06-17 13:51:22 +0200

    fix emitter trafo

--- decode/utils/emitter_trafo.py
+++ decode/utils/emitter_trafo.py
@@ -1,45 +1,43 @@
 import torch
 from ..emitter.emitter import EmitterSet
 
-"""
-Predefined transformations, e.g. for the SMLM challenge.
-Factors and shifts are with respect to (possibly) changed axis, i.e. in the new system.
-"""
+# Predefined transformations, e.g. for the SMLM challenge.
+# Factors and shifts are with respect to (possibly) changed axis, i.e. in the new system.
 
 challenge_import = {
-    'desc': 'Challenge data transformation to match DECODE (this framework) format.',
-    'xy_unit': 'nm',
-    'px_size': (100., 100.),
-    'xyz_axis': (1, 0, 2),
-    'xyz_nm_factor': (1., 1., -1.),
-    'xyz_nm_shift': (-150., -50., 0.),
-    'xyz_px_factor': None,
-    'xyz_px_shift': None,
-    'frame_ix_shift': -1
+    "desc": "Challenge data transformation to match DECODE (this framework) format.",
+    "xy_unit": "nm",
+    "px_size": (100.0, 100.0),
+    "xyz_axis": (1, 0, 2),
+    "xyz_nm_factor": (1.0, 1.0, -1.0),
+    "xyz_nm_shift": (-150.0, -50.0, 0.0),
+    "xyz_px_factor": None,
+    "xyz_px_shift": None,
+    "frame_ix_shift": -1,
 }
 
 challenge_export = {
-    'desc': 'DECODE (this framework) data transformation to match the challenge format.',
-    'xy_unit': 'nm',
-    'px_size': (100., 100.),
-    'xyz_axis': (1, 0, 2),
-    'xyz_nm_factor': (1., 1., -1.),
-    'xyz_nm_shift': (50., 150., 0.),
-    'xyz_px_factor': None,
-    'xyz_px_shift': None,
-    'frame_ix_shift': +1
+    "desc": "DECODE (this framework) data transformation to match the challenge format.",
+    "xy_unit": "nm",
+    "px_size": (100.0, 100.0),
+    "xyz_axis": (1, 0, 2),
+    "xyz_nm_factor": (1.0, 1.0, -1.0),
+    "xyz_nm_shift": (50.0, 150.0, 0.0),
+    "xyz_px_factor": None,
+    "xyz_px_shift": None,
+    "frame_ix_shift": +1,
 }
 
 deepstorm3d_import = {
-    'desc': 'Transformation of DeepStorm output files to be compatible to DECODE.',
-    'xy_unit': 'px',
-    'px_size': None,
-    'xyz_axis': (1, 0, 2),
-    'xyz_nm_factor': None,
-    'xyz_nm_shift': None,
-    'xyz_px_factor': None,
-    'xyz_px_shift': None,
-    'frame_ix_shift': -1
+    "desc": "Transformation of DeepStorm output files to be compatible to DECODE.",
+    "xy_unit": "px",
+    "px_size": None,
+    "xyz_axis": (1, 0, 2),
+    "xyz_nm_factor": None,
+    "xyz_nm_shift": None,
+    "xyz_px_factor": None,
+    "xyz_px_shift": None,
+    "frame_ix_shift": -1,
 }
 
 
@@ -52,38 +50,43 @@ def transform_emitter(em: EmitterSet, trafo: dict) -> EmitterSet:
         trafo: transformation specs
 
     """
-
     mod_em = em.clone()
 
-    """Set Px Size"""
-    mod_em.px_size = torch.tensor(trafo['px_size']) if trafo['px_size'] is not None else mod_em.px_size
+    # set Px Size
+    mod_em.px_size = (
+        torch.tensor(trafo["px_size"])
+        if trafo["px_size"] is not None
+        else mod_em.px_size
+    )
 
-    """Modify proper attributes"""
-    if trafo['xyz_axis'] is not None:
-        mod_em.xyz = mod_em.xyz[:, trafo['xyz_axis']]
-        mod_em.xyz_cr = mod_em.xyz_cr[:, trafo['xyz_axis']]
-        mod_em.xyz_sig = mod_em.xyz_sig[:, trafo['xyz_axis']]
+    # modify proper attributes
+    if trafo["xyz_axis"] is not None:
+        mod_em.xyz = mod_em.xyz[:, trafo["xyz_axis"]]
+        if em.xyz_cr is not None:
+            mod_em.xyz_cr = mod_em.xyz_cr[:, trafo["xyz_axis"]]
+        if em.xyz_sig is not None:
+            mod_em.xyz_sig = mod_em.xyz_sig[:, trafo["xyz_axis"]]
 
-    if trafo['xyz_nm_factor'] is not None:
-        mod_em.xyz_nm *= torch.tensor(trafo['xyz_nm_factor'])
+    if trafo["xyz_nm_factor"] is not None:
+        mod_em.xyz_nm *= torch.tensor(trafo["xyz_nm_factor"])
 
-    if trafo['xyz_nm_shift'] is not None:
-        mod_em.xyz_nm += torch.tensor(trafo['xyz_nm_shift'])
+    if trafo["xyz_nm_shift"] is not None:
+        mod_em.xyz_nm += torch.tensor(trafo["xyz_nm_shift"])
 
-    if trafo['xyz_px_factor'] is not None:
-        mod_em.xyz_px *= torch.tensor(trafo['xyz_px_factor'])
+    if trafo["xyz_px_factor"] is not None:
+        mod_em.xyz_px *= torch.tensor(trafo["xyz_px_factor"])
 
-    if trafo['xyz_px_shift'] is not None:
-        mod_em.xyz_px += torch.tensor(trafo['xyz_px_shift'])
+    if trafo["xyz_px_shift"] is not None:
+        mod_em.xyz_px += torch.tensor(trafo["xyz_px_shift"])
 
-    if trafo['frame_ix_shift'] is not None:
-        mod_em.frame_ix += torch.tensor(trafo['frame_ix_shift'])
+    if trafo["frame_ix_shift"] is not None:
+        mod_em.frame_ix += torch.tensor(trafo["frame_ix_shift"])
 
-    """Modify unit in which emitter is stored and possibly set px size'"""
-    if trafo['xy_unit'] is not None:
-        if trafo['xy_unit'] == 'nm':
+    # modify unit in which emitter is stored and possibly set px size
+    if trafo["xy_unit"] is not None:
+        if trafo["xy_unit"] == "nm":
             mod_em.xyz_nm = mod_em.xyz_nm
-        elif trafo['xy_unit'] == 'px':
+        elif trafo["xy_unit"] == "px":
             mod_em.xyz_px = mod_em.xyz_px
         else:
             raise ValueError(f"Unsupported unit ({trafo['xy_unit']}).")
--- test/unit/utils/test_emittertrafo.py
+++ test/unit/utils/test_emittertrafo.py
@@ -1,24 +1,22 @@
+import numpy as np
 import torch
 
-import decode
-from decode.generic import test_utils
 from decode.emitter.emitter import factory
 from decode.utils import emitter_trafo
 
 
 def test_emitter_transformation():
-    """Setup"""
-    em = factory(200, extent=10000, xy_unit='px', px_size=(100., 100.))
+    em = factory(200, extent=10000, xy_unit="px", px_size=(100.0, 100.0))
     em.frame_ix = torch.randint_like(em.frame_ix, 1, 10000)
 
-    """Run"""
     mod_em = emitter_trafo.transform_emitter(em, emitter_trafo.challenge_import)
 
-    """Assert"""
     assert id(em) != id(mod_em), "Emitter transformation should return a new object."
-    assert mod_em.xy_unit == 'nm'
-    assert test_utils.tens_almeq(mod_em.px_size, torch.tensor([100., 100.]))
+    assert not mod_em.eq_meta(em)
 
     assert mod_em.frame_ix.min() == em.frame_ix.min() - 1
-    assert test_utils.tens_almeq(mod_em.xyz_nm, em.xyz_nm[:, [1, 0, 2]] * torch.tensor([1., 1., -1.]) + torch.tensor(
-        [-150., -50., 0.]))
+    np.testing.assert_array_equal(
+        mod_em.xyz_nm,
+        em.xyz_nm[:, [1, 0, 2]] * torch.tensor([1.0, 1.0, -1.0])
+        + torch.tensor([-150.0, -50.0, 0.0]),
+    )
