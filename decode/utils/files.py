13d33cbc61edac4e6fff6e1ed6186db24857be99
Lucas-Raphael Mueller <lucas.mueller@embl.de>
2022-07-11 13:14:51 +0200

    rename file / path

--- decode/utils/files.py
+++ decode/utils/files.py
@@ -5,17 +5,17 @@ import shutil
 from pathlib import Path
 
 
-def check_file(file: (str, pathlib.Path), hash=None) -> bool:
+def check_file(path: (str, pathlib.Path), hash=None) -> bool:
 
-    if not isinstance(file, pathlib.Path):
-        file = pathlib.Path(file)
+    if not isinstance(path, pathlib.Path):
+        path = pathlib.Path(path)
 
-    if file.exists():
+    if path.exists():
 
         if hash is None:
             return True
         else:
-            if hash == hashlib.sha256(file.read_bytes()).hexdigest():
+            if hash == hashlib.sha256(path.read_bytes()).hexdigest():
                 return True
             else:
                 return False
@@ -24,64 +24,64 @@ def check_file(file: (str, pathlib.Path), hash=None) -> bool:
         return False
 
 
-def load(file: (str, pathlib.Path), url: str, hash: str = None) -> None:
+def load(path: (str, pathlib.Path), url: str, hash: str = None) -> None:
     """
     Loads file from URL (and checks hash if present)
 
     Args:
-        file: path where to store downloaded file
+        path: path where to store downloaded file
         url:
         hash:
 
     """
 
-    if not isinstance(file, pathlib.Path):
-        file = pathlib.Path(file)
+    if not isinstance(path, pathlib.Path):
+        path = pathlib.Path(path)
 
     file_www = requests.get(url)
     file_www.raise_for_status()  # raises an error if the file is not available
 
-    with file.open("wb") as f:
+    with path.open("wb") as f:
         f.write(file_www.content)
 
-    d_hash = hashlib.sha256(file.read_bytes()).hexdigest()
+    d_hash = hashlib.sha256(path.read_bytes()).hexdigest()
     if hash is not None and not hash == d_hash:
         raise RuntimeError(
             f"Downloaded file does not match hash.\nSHA-256 of ref.: {hash}\nSHA-256 of downloaded: {d_hash}"
         )
 
 
-def check_load(file: (str, pathlib.Path), url: str, hash: str = None):
+def check_load(path: (str, pathlib.Path), url: str, hash: str = None):
     """
     Loads file freshly when check fails
 
     Args:
-        file:
+        path:
         url:
         hash:
 
     """
 
-    if not check_file(file, hash):
-        load(file, url, hash)
+    if not check_file(path, hash):
+        load(path, url, hash)
 
 
-def hash_file(file: pathlib.Path) -> str:
+def hash_file(path: pathlib.Path) -> str:
     """
     Hash a file. Reads everything in byte mode even if it is a text file.
 
     Args:
-        file (pathlib.Path): full path to file
+        path (pathlib.Path): full path to file
 
     Returns:
         str: hexdigest sha256 hash
 
     """
 
-    if not file.exists():
-        raise FileExistsError(f"File {str(file)} does not exist.")
+    if not path.exists():
+        raise FileExistsError(f"File {str(path)} does not exist.")
 
-    return hashlib.sha256(file.read_bytes()).hexdigest()
+    return hashlib.sha256(path.read_bytes()).hexdigest()
 
 
 class AutoRemove:
