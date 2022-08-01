8a632098c1de0816a74af6e464736cecf023c747
Lucas-Raphael Mueller <lucas.mueller@embl.de>
2022-08-01 13:03:27 +0200

    Add version of docs to bump2version and clean up conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,42 +12,43 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 import decode
 
-project = 'DECODE'
-copyright = '2020, Artur Speiser, Lucas-Raphael Mueller et al.'
-author = 'Artur Speiser, Lucas-Raphael Mueller et al.'
+project = "DECODE"
+copyright = "2020, Artur Speiser, Lucas-Raphael Mueller et al."
+author = "Artur Speiser, Lucas-Raphael Mueller et al."
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.a'
+release = "0.10.1dev1"
 
 
 # -- General configuration ---------------------------------------------------
 
 source_parsers = {
-    '.md': 'recommonmark.parser.CommonMarkParser',
+    ".md": "recommonmark.parser.CommonMarkParser",
 }
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_autodoc_typehints',
-    'sphinx.ext.viewcode',
-    'sphinx_markdown_tables',
-    'recommonmark',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.viewcode",
+    "sphinx_markdown_tables",
+    "recommonmark",
 ]
 
 # napoleon
 napoleon_use_param = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -57,8 +58,8 @@ exclude_patterns = []
 
 # map file extension to respective types
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".md": "markdown",
 }
 
 
@@ -81,7 +82,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # which class content to include
-autoclass_content = 'init'  # init
+autoclass_content = "init"  # init
