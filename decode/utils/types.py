1754a80fa05a084dee4332e25b8e74bd349dddaa
Lucas-Raphael Mueller <lucas.mueller@embl.de>
2022-08-01 14:02:14 +0200

    Clean up

--- decode/utils/types.py
+++ decode/utils/types.py
@@ -3,25 +3,25 @@ from types import SimpleNamespace
 
 
 class RecursiveNamespace(SimpleNamespace):
-    """
-    Extension of SimpleNamespace to recursive dictionaries.
-    Inspired by https://dev.to/taqkarim/extending-simplenamespace-for-nested-dictionaries-58e8
-    """
-
-    @staticmethod
-    def map_entry(entry):
-        if isinstance(entry, dict):
-            return RecursiveNamespace(**entry)
-
-        return entry
-
     def __init__(self, **kwargs):
+        """
+        Extension of SimpleNamespace to recursive dictionaries.
+        Inspired by https://dev.to/taqkarim/extending-simplenamespace-for-nested-dictionaries-58e8
+        """
+
         super().__init__(**kwargs)
         for key, val in kwargs.items():
             if isinstance(val, dict):
                 setattr(self, key, RecursiveNamespace(**val))
             elif isinstance(val, (tuple, list)):
                 setattr(self, key, list(map(self.map_entry, val)))
+
+    def __getitem__(self, item):
+        out = getattr(self, item)
+        if isinstance(out, RecursiveNamespace):
+            raise TypeError
+
+        return out
 
     def to_dict(self):
         d = deepcopy(self.__dict__)  # should not be inplace, otherwise shitty
@@ -32,12 +32,12 @@ class RecursiveNamespace(SimpleNamespace):
 
         return d
 
-    def __getitem__(self, item):
-        out = getattr(self, item)
-        if isinstance(out, RecursiveNamespace):
-            raise TypeError
-
-        return out
-
     def keys(self):
         return self.__dict__.keys()
+
+    @staticmethod
+    def map_entry(entry):
+        if isinstance(entry, dict):
+            return RecursiveNamespace(**entry)
+
+        return entry
