966beceebda502117e4a236fecd0375c88c416e3
Lucas-Raphael Mueller <git@lrm.dev>
2022-10-04 10:12:15 +0200

    Add paths to trainable config

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -2,7 +2,8 @@ import pytest
 from pathlib import Path
 from omegaconf import OmegaConf, DictConfig
 
-from decode import io
+from decode import io, utils
+from decode.generic import asset_handler
 
 
 @pytest.fixture
@@ -38,12 +39,31 @@ def cfg() -> DictConfig:
 
 
 @pytest.fixture
-def cfg_trainable(cfg) -> DictConfig:
-    # ToDo: A trainable cfg, i.e. all necessary assets and directories present
+def cfg_trainable(cfg, tmpdir) -> DictConfig:
+    """
+    A trainable config. Successively fill out all parameters necessary for training.
 
+    Args:
+        cfg: reference config
+    """
+    cfg = utils.types.RecursiveNamespace(**OmegaConf.to_object(cfg))
+
+    cfg.Simulation.bg_uniform = (10., 100.)
     cfg.Simulation.intensity.mean = 5000
     cfg.Simulation.intensity.std = 1000
-
     cfg.Simulation.lifetime_avg = 1.
+
+    # Todo: Replace scaling by auto
+    cfg.Scaling.input_scale = 100.
+    cfg.Scaling.input_offset = 10.
+    cfg.Scaling.bg_max = 120.
+    cfg.Scaling.phot_max = 13000.
+    cfg.Scaling.z_max = 900.
+
+    cfg.Paths.calibration = asset_handler.load_asset("bead_cal")
+    cfg.Paths.experiment = tmpdir / "model"
+    cfg.Paths.logging = tmpdir / "log"
+
+    cfg.Trainer.max_epochs = 3
 
     return cfg
