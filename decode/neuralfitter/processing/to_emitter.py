f4cf324d5bc35fbf155a30a8581d43b744eec14a
Lucas-Raphael Mueller <git@lrm.dev>
2022-10-07 13:02:39 +0200

    Refactor post-processing and split into two classes ToEmitter and PostProcessing.

--- decode/neuralfitter/processing/to_emitter.py
+++ decode/neuralfitter/processing/to_emitter.py
@@ -51,7 +51,7 @@ class ToEmitterLookUpPixelwise(ToEmitter):
         """
         super().__init__(xy_unit=xy_unit, px_size=px_size)
 
-        self._mask_impl = mask
+        self._mask = mask
         self._pphotxyzbg_mapping = pphotxyzbg_mapping
         self._photxyz_sigma_mapping = photxyz_sigma_mapping
         self._device = device
@@ -59,14 +59,14 @@ class ToEmitterLookUpPixelwise(ToEmitter):
         assert len(self._pphotxyzbg_mapping) == 6, "Wrong length of mapping."
         if self._photxyz_sigma_mapping is not None:
             assert (
-                    len(self._photxyz_sigma_mapping) == 4
+                len(self._photxyz_sigma_mapping) == 4
             ), "Wrong length of sigma mapping."
 
-    def _mask(self, p: torch.Tensor) -> torch.BoolTensor:
-        if not callable(self._mask_impl):
-            return p >= self._mask_impl
+    def _mask_impl(self, p: torch.Tensor) -> torch.BoolTensor:
+        if not callable(self._mask):
+            return p >= self._mask
         else:
-            return self._mask_impl(p)
+            return self._mask(p)
 
     @staticmethod
     def _lookup_features(
@@ -104,7 +104,7 @@ class ToEmitterLookUpPixelwise(ToEmitter):
         # reorder features channel-wise
         x_mapped = x[:, self._pphotxyzbg_mapping]
 
-        active_px = self._mask(x_mapped[:, 0])  # 0th ch. is detection channel
+        active_px = self._mask_impl(x_mapped[:, 0])  # 0th ch. is detection channel
         prob = x_mapped[:, 0][active_px]
 
         frame_ix, features = self._lookup_features(x_mapped[:, 1:], active_px)
@@ -135,5 +135,119 @@ class ToEmitterLookUpPixelwise(ToEmitter):
         )
 
 
-class ToEmitterSpatialIntegration(ToEmitter):
-    pass
+def _norm_sum(*args):
+    return torch.clamp(torch.add(*args), 0.0, 1.0)
+
+
+class SpatialIntegration(ToEmitterLookUpPixelwise):
+    _p_aggregations = ("sum", "norm_sum")  # , 'max', 'pbinom_cdf', 'pbinom_pdf')
+
+    def __init__(
+        self,
+        raw_th: float,
+        xy_unit: str,
+        px_size=None,
+        pphotxyzbg_mapping: Union[list, tuple] = (0, 1, 2, 3, 4, -1),
+        photxyz_sigma_mapping: Union[list, tuple, None] = (5, 6, 7, 8),
+        p_aggregation: Union[str, Callable] = "norm_sum",
+        _split_th: float = 0.6,
+    ):
+        """
+        Spatial Integration to handle local clusters of non-zero probability.
+        The procedure is to cut off low probability predictions, get localizations with
+        high probabily which are used as is, but for local clusters find maxima and sum
+        up their probabilites.
+
+        Args:
+            raw_th: probability threshold from where detections are considered
+            xy_unit: unit of the xy coordinates
+            px_size: pixel size
+            pphotxyzbg_mapping: channel index mapping
+            photxyz_sigma_mapping: channel index mapping of sigma channels
+            p_aggregation: aggreation method to aggregate probabilities.
+             can be 'sum', 'max', 'norm_sum'
+            _split_th: threshold above which a prediction is treated as `easy` in that
+             it is a clear prediction.
+        """
+        super().__init__(
+            mask=raw_th,
+            xy_unit=xy_unit,
+            px_size=px_size,
+            pphotxyzbg_mapping=pphotxyzbg_mapping,
+            photxyz_sigma_mapping=photxyz_sigma_mapping,
+        )
+        self._raw_th = raw_th
+        self._split_th = _split_th
+        self._p_aggregation = self._set_p_aggregation(p_aggregation)
+
+    def forward(self, x: torch.Tensor) -> emitter.EmitterSet:
+        x[:, 0] = self._non_max_suppression(x[:, 0])
+        return super().forward(x)
+
+    def _non_max_suppression(self, p: torch.Tensor) -> torch.Tensor:
+        """
+
+        Args:
+            p: probability map
+        """
+        with torch.no_grad():
+            p_copy = p.clone()
+
+            # Probability values > 0.3 are regarded as possible locations
+            p_clip = torch.where(p > self._raw_th, p, torch.zeros_like(p))[:, None]
+
+            # localize maximum values within a 3x3 patch
+            pool = torch.nn.functional.max_pool2d(p_clip, 3, 1, padding=1)
+            max_mask1 = torch.eq(p[:, None], pool).float()
+
+            # Add probability values from the 4 adjacent pixels
+            diag = 0.0  # 1/np.sqrt(2)
+            filt = (
+                torch.tensor([[diag, 1.0, diag], [1, 1, 1], [diag, 1, diag]])
+                .unsqueeze(0)
+                .unsqueeze(0)
+                .to(p.device)
+            )
+            conv = torch.nn.functional.conv2d(p[:, None], filt, padding=1)
+            p_ps1 = max_mask1 * conv
+
+            # to identify two fluorophores in adjacent pixels we look
+            # for probablity values > 0.6 that are not part of the first mask
+            p_copy *= 1 - max_mask1[:, 0]
+            # p_clip = torch.where(p_copy > split_th, p_copy, torch.zeros_like(p_copy))[:, None]
+            max_mask2 = torch.where(
+                p_copy > self._split_th,
+                torch.ones_like(p_copy),
+                torch.zeros_like(p_copy),
+            )[:, None]
+            p_ps2 = max_mask2 * conv
+
+            # this is our final clustered probablity which we then threshold
+            # (normally > 0.7) to get our final discrete locations
+            p_ps = self._p_aggregation(p_ps1, p_ps2)
+            assert p_ps.size(1) == 1
+
+            return p_ps.squeeze(1)
+
+    @classmethod
+    def _set_p_aggregation(cls, p_aggr: Union[str, Callable]) -> Callable:
+        """
+        Returns the p_aggregation callable
+
+        Args:
+            p_aggr: probability aggregation
+        """
+
+        if isinstance(p_aggr, str):
+
+            if p_aggr == "sum":
+                return torch.add
+            elif p_aggr == "max":
+                return torch.max
+            elif p_aggr == "norm_sum":
+                return _norm_sum
+            else:
+                raise ValueError
+
+        else:
+            return p_aggr
--- test/unit/neuralfitter/processing/test_to_emitter.py
+++ test/unit/neuralfitter/processing/test_to_emitter.py
@@ -74,11 +74,13 @@ class TestLookUpPostProcessing(TestToEmitter):
 
     def test_filter(self, post):
         detection = torch.tensor([[0.1, 0.0], [0.6, 0.05]]).unsqueeze(0)
-        active_px = post._mask(detection)
+        active_px = post._mask_impl(detection)
         assert (active_px == torch.tensor([[1, 0], [1, 0]]).unsqueeze(0).bool()).all()
 
     def test_lookup(self, post):
-        active_px = post._mask(torch.tensor([[0.1, 0.0], [0.6, 0.05]]).unsqueeze(0))
+        active_px = post._mask_impl(
+            torch.tensor([[0.1, 0.0], [0.6, 0.05]]).unsqueeze(0)
+        )
 
         features = (
             torch.tensor([[1.0, 2.0], [3.0, 4.0]])
@@ -100,8 +102,8 @@ class TestLookUpPostProcessing(TestToEmitter):
 
         # This is hard coded designed for the very specific test case
         assert (
-                (features / (torch.arange(5).unsqueeze(1).float() + 1)).unique()
-                == torch.tensor([1.0, 3.0])
+            (features / (torch.arange(5).unsqueeze(1).float() + 1)).unique()
+            == torch.tensor([1.0, 3.0])
         ).all()
 
     def test_forward_no_sigma(self, post, pseudo_out_no_sigma):
@@ -137,3 +139,65 @@ class TestLookUpPostProcessing(TestToEmitter):
         np.testing.assert_array_almost_equal(
             emitter_out.phot_sig, torch.tensor([10.0, 1 / 0.6])
         )
+
+
+class TestSpatialIntegration(TestLookUpPostProcessing):
+    @pytest.fixture
+    def post(self):
+        return to_emitter.SpatialIntegration(raw_th=0.1, xy_unit="px")
+
+    def test_forward_no_sigma(self, post, pseudo_out_no_sigma):
+        post._photxyz_sigma_mapping = None
+
+        emitter_out = post.forward(pseudo_out_no_sigma)
+
+        assert isinstance(
+            emitter_out, emitter.EmitterSet
+        ), "Output should be an emitter."
+        assert len(emitter_out) == 1
+        assert emitter_out.frame_ix == 0
+        assert emitter_out.phot == pytest.approx(3.0)
+        assert emitter_out.prob == pytest.approx(0.75)
+
+    def test_forward(self, post, pseudo_out):
+        emitter_out = post.forward(pseudo_out)
+
+        assert isinstance(emitter_out, emitter.EmitterSet)
+        assert len(emitter_out) == 1
+
+        assert not torch.isnan(
+            emitter_out.xyz_sig
+        ).any(), "Sigma values for xyz should not be nan."
+        assert not torch.isnan(
+            emitter_out.phot_sig
+        ).any(), "Sigma values for phot should not be nan."
+        assert emitter_out.bg_sig is None
+
+        np.testing.assert_allclose(
+            emitter_out.xyz_sig, torch.tensor([[2 / 0.6, 3 / 0.6, 4 / 0.6]])
+        )
+        np.testing.assert_allclose(emitter_out.phot_sig, torch.tensor([1 / 0.6]))
+
+    @pytest.mark.parametrize(
+        "aggr,expct",
+        [
+            ("sum", ([0.0, 1.000001], [0.0, 0.501])),
+            ("norm_sum", ([0.0, 1.0], [0.0, 0.501])),
+        ],
+    )
+    def test_nms(self, post, aggr, expct):
+        post.p_aggregation = post._set_p_aggregation(aggr)
+        post._raw_th = 0.2
+        post._split_th = 0.6
+
+        p = torch.zeros((2, 32, 32))
+        p[0, 4:6, 4] = 0.5
+        p[0, 4, 4] = 0.5
+        p[0, 5, 4] = 0.500001
+        p[1, 6, 4] = 0.25
+        p[1, 7, 4] = 0.251
+
+        p_out = post._non_max_suppression(p)
+
+        np.testing.assert_allclose(p_out[0, 4:6, 4], torch.tensor(expct[0]), atol=1e-5)
+        np.testing.assert_allclose(p_out[1, 6:8, 4], torch.tensor(expct[1]), atol=1e-5)
