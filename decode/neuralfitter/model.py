4300fc101b8b19e13e9bbf2f51eafca407500992
Lucas-Raphael Mueller <git@lrm.dev>
2022-10-13 17:05:53 +0200

    Sync model and test

--- decode/neuralfitter/model.py
+++ decode/neuralfitter/model.py
@@ -1,20 +1,26 @@
-from typing import Optional
+from typing import Optional, Protocol
 
 import pytorch_lightning as pl
 import torch
 
+from decode.emitter import emitter
+from decode.neuralfitter import logger, process
 
-from decode.neuralfitter import process
+
+class _EvaluatorEmitter(Protocol):
+    def forward(self, out: emitter.EmitterSet, ref: emitter.EmitterSet) -> dict:
+        pass
 
 
 class Model(pl.LightningModule):
     def __init__(
-            self,
-            model: torch.nn.Module,
-            optimizer: torch.optim.Optimizer,
-            loss: torch.nn.Module,
-            proc: process.Processing,
-            evaluator: Optional,
+        self,
+        model: torch.nn.Module,
+        optimizer: torch.optim.Optimizer,
+        loss: torch.nn.Module,
+        proc: process.Processing,
+        evaluator: Optional[_EvaluatorEmitter],
+        batch_size: int,
     ):
         super().__init__()
 
@@ -23,8 +29,13 @@ class Model(pl.LightningModule):
         self._proc = proc
         self._loss = loss
         self._evaluator = evaluator
+        self.batch_size = batch_size
         self._em_val_out = []
         self._em_val_tar = []
+
+    @property
+    def logger(self) -> Optional[logger.Logger]:
+        return super().logger
 
     def configure_optimizers(self):
         return self._opt
@@ -34,9 +45,12 @@ class Model(pl.LightningModule):
 
         y_out = self._model.forward(x)
         y_out = self._proc.post_model(y_out)
-        loss = self._loss.forward(y, y_out)
+        loss, loggable = self._loss.forward(y_out, y)
 
-        self.log("loss/train", loss, on_step=True, on_epoch=True)
+        self.log(
+            "loss/train", loss, on_step=True, on_epoch=True, batch_size=self.batch_size
+        )
+        self.logger.log_group(loggable, prefix="loss_cmpt/")
 
         return loss
 
@@ -45,15 +59,18 @@ class Model(pl.LightningModule):
 
         y_out = self._model.forward(x)
         y_out_proc = self._proc.post_model(y_out)
-        loss = self._loss.forward(y_out_proc, y_ref)
+        loss, loggable = self._loss.forward(y_out_proc, y_ref)
+
+        self.log(
+            "loss/val", loss, on_step=True, on_epoch=True, batch_size=self.batch_size
+        )
+        self.logger.log_group(loggable, prefix="loss_cmpt/")
 
         em_out = self._proc.post(y_out)
-        em_out.frame_ix += batch_ix * self.trainer.batch_size
+        em_out.frame_ix += batch_ix * self.batch_size
 
         self._em_val_out.append(em_out)
         self._em_val_tar.append(y_em_val)
-
-        self.log("loss/val", loss, on_step=True, on_epoch=True)
 
         return loss
 
@@ -61,5 +78,8 @@ class Model(pl.LightningModule):
         if self._evaluator is None:
             return
 
-        metrics = self._evaluator(self._em_val_tar, self._em_val_out)
-        self.log_dict(metrics, on_epoch=True)
+        em_out = emitter.EmitterSet.cat(self._em_val_out)
+        em_tar = emitter.EmitterSet.cat(self._em_val_tar)
+
+        metrics = self._evaluator.forward(em_out, em_tar)
+        self.logger.log_group(metrics)
--- test/unit/neuralfitter/test_model.py
+++ test/unit/neuralfitter/test_model.py
@@ -9,39 +9,47 @@ from decode.neuralfitter import model
 
 @pytest.fixture
 def mock_model():
+    model_bb = mock.MagicMock()
+    model_bb.forward.return_value = torch.rand(2, 10, 16, 16)
+
     loss = mock.MagicMock()
-    loss.forward.return_value = torch.tensor(17.)
+    loss.forward.return_value = torch.tensor(17.), {"a": 17}
 
     evaluator = mock.MagicMock()
     evaluator.return_value = {"rmse": 17., "mad": 42.}
 
-    return model.Model(
-        model=mock.MagicMock(),
+    m = model.Model(
+        model=model_bb,
         loss=loss,
         proc=mock.MagicMock(),
-        em_val_tar=mock.MagicMock(),
         evaluator=evaluator,
+        optimizer=mock.MagicMock(),
+        batch_size=16,
     )
+    m.log = mock.MagicMock()
+    m.trainer = mock.MagicMock()
+    m.trainer.loggers = [mock.MagicMock()]
+    return m
 
 
-@pytest.mark.parametrize("method", ["training_step", "validation_step"])
-def test_model_train_val_shared(method, mock_model):
+def test_model_training_step(mock_model):
     x, y = mock.MagicMock(), mock.MagicMock()
 
-    loss = getattr(mock_model, method)((x, y), 42)
+    loss = mock_model.training_step((x, y), 42)
     assert loss == 17.
     mock_model._model.forward.assert_called_once_with(x)
     mock_model._proc.post_model.assert_called_once_with(
         mock_model._model.forward.return_value
     )
     mock_model._loss.forward.assert_called_once_with(
-        y,
         mock_model._proc.post_model.return_value,
+        y,
     )
 
 
 def test_model_validation(mock_model):
     x, y = mock.MagicMock(), mock.MagicMock()
+    y_em = emitter.factory(0)
 
     em_dummy_out = emitter.factory(frame_ix=[1, 2])
     em_dummy_tar = em_dummy_out.clone()
@@ -49,10 +57,13 @@ def test_model_validation(mock_model):
 
     mock_model._proc.post.return_value = em_dummy_out
 
-    _ = mock_model.validation_step((x, y), 42)
+    _ = mock_model.validation_step((x, y, y_em), 42)
     assert mock_model._em_val_out == [em_dummy_out]
 
 
 def test_model_on_validation_epoch_end(mock_model):
+    mock_model._em_val_out = [emitter.factory(0)]
+    mock_model._em_val_tar = [emitter.factory(0)]
+
     mock_model.on_validation_epoch_end()
-    mock_model._evaluator.assert_called_once()
+    mock_model._evaluator.forward.assert_called_once()
