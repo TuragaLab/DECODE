a0c00be7672f24f80dee288f7363cee8c5606c61
Lucas-Raphael Mueller <git@lrm.dev>
2022-11-24 21:34:11 +0100

    style

--- test/unit/neuralfitter/test_spec.py
+++ test/unit/neuralfitter/test_spec.py
@@ -15,10 +15,13 @@ def test_spec_n(n_code, n):
     assert s.n_bg == 1
 
 
-@pytest.mark.parametrize("n_code,ix_prob,ix_mu,ix_sig,ix_bg,ix_phot,ix_xyz,ix_phot_sig", [
-    (1, [0], [1, 2, 3, 4], [5, 6, 7, 8], [9], [1], [2, 3, 4], [5]),
-    (3, [0, 1, 2], [3, 4, 5, 6], [7, 8, 9, 10], [11], [3], [4, 5, 6], [7]),
-])
+@pytest.mark.parametrize(
+    "n_code,ix_prob,ix_mu,ix_sig,ix_bg,ix_phot,ix_xyz,ix_phot_sig",
+    [
+        (1, [0], [1, 2, 3, 4], [5, 6, 7, 8], [9], [1], [2, 3, 4], [5]),
+        (3, [0, 1, 2], [3, 4, 5, 6], [7, 8, 9, 10], [11], [3], [4, 5, 6], [7]),
+    ],
+)
 def test_spec_ix(n_code, ix_prob, ix_mu, ix_sig, ix_bg, ix_phot, ix_xyz, ix_phot_sig):
     s = spec.ModelChannelMapGMM(n_codes=n_code)
 
