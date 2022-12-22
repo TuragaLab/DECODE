db94d1f2d0a18c109499fc4180407fd5dab3962d
Lucas-Raphael Mueller <lucas.mueller@embl.de>
2022-12-22 16:54:41 +0100

    add auto config

--- decode/neuralfitter/train/train.py
+++ decode/neuralfitter/train/train.py
@@ -3,15 +3,23 @@ from pathlib import Path
 
 import argparse
 import hydra
+import omegaconf
 import pytorch_lightning as pl
 import torch
 
-from . import setup_cfg
+from decode.neuralfitter.train import setup_cfg
 from decode.neuralfitter import model
 from decode.neuralfitter.data import datamodel
+from decode.utils import param_auto
 
 
-def train(cfg):
+def train(cfg: omegaconf.DictConfig):
+    auto_cfg = param_auto.AutoConfig(
+        fill=True, fill_test=True, auto_scale=True, return_type=omegaconf.DictConfig
+    )
+    cfg = omegaconf.OmegaConf.to_container(cfg)
+    cfg = auto_cfg.parse(cfg)
+
     if (cs := cfg.Computing.multiprocessing_sharing_strategy) is not None:
         torch.multiprocessing.set_sharing_strategy(cs)
 
@@ -36,13 +44,25 @@ def train(cfg):
         evaluator=evaluator,
         batch_size=cfg.Trainer.batch_size,
     )
+    m_ckpt = pl.callbacks.ModelCheckpoint(dirpath=cfg.Paths.experiment)
+    logger = setup_cfg.setup_logger(cfg)
 
     trainer = pl.Trainer(
+        default_root_dir=cfg.Paths.experiment,
         accelerator="gpu" if "cuda" in cfg.Hardware.device.training else "cpu",
         precision=cfg.Computing.precision,
-        reload_dataloaders_every_n_epochs=1
+        reload_dataloaders_every_n_epochs=1,
+        logger=logger,
+        max_epochs=cfg.Trainer.max_epochs,
+        callbacks=[m_ckpt],
     )
-    trainer.fit(model=m, datamodule=dm)
+    trainer.fit(
+        model=m,
+        datamodule=dm,
+        ckpt_path=cfg.Paths.checkpoint_init,  # resumes training if not None
+    )
+
+    print("Done")
 
 
 def parse_args():
@@ -88,11 +108,14 @@ if __name__ == "__main__":
     args = parse_args()
 
     # hack hydra and fake sys args
-    argv = sys.argv
-    sys.argv = [argv[0], argv[-1].strip("--overwrites=")]
-    #argv = [a for a in sys.argv if (c not in a for c in {"-c=", "--config="})]
+    argv = [sys.argv[0]]
+    if args.overwrites is not None:
+        argv.extend(args.overwrites.split(" "))
+    sys.argv = argv
 
     config_file = Path(args.config)
     config_dir = config_file.parent
-    train_wrapped = hydra.main(config_path=config_dir, config_name=config_file.stem)(train)
+    train_wrapped = hydra.main(config_path=config_dir, config_name=config_file.stem)(
+        train
+    )
     train_wrapped()
