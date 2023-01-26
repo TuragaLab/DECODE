fcc2b70f9e4b4ee70652597ed52e648d2ced7af4
Lucas-Raphael Mueller <lucas.mueller@embl.de>
2023-01-26 16:58:06 +0100

    Generalise post-processing by channel map

--- test/unit/neuralfitter/processing/test_post.py
+++ test/unit/neuralfitter/processing/test_post.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 
 from decode.emitter import emitter
+from decode.neuralfitter import spec
 from decode.neuralfitter.processing import post as post_processing
 from decode.neuralfitter import scale_transform, coord_transform
 from decode.neuralfitter.processing import to_emitter
@@ -27,18 +28,43 @@ class TestPostProcessingAbstract:
 class TestPostProcessingGaussianMixture(TestPostProcessingAbstract):
     @pytest.fixture
     def post(self):
-        scaler = scale_transform.ScalerModelOutput(1., 2., 3.)
+        ch_map = spec.ModelChannelMapGMM(3)
+        scaler = scale_transform.ScalerModelChannel.from_ch_spec(
+            ch_map=ch_map,
+            phot=10000.,
+            z=800.,
+            bg=100.,
+            sigma_factor=1.,
+            sigma_eps=0.,
+        )
         coord = coord_transform.Offset2Coordinate((-0.5, 31.5), (-0.5, 31.5), (32, 32))
-        frame2em = to_emitter.ToEmitterLookUpPixelwise(.6, xy_unit="px")
+        frame2em = to_emitter.ToEmitterLookUpPixelwise(.6, ch_map=ch_map, xy_unit="px")
 
-        return post_processing.PostProcessingGaussianMixture(scaler, coord, frame2em)
+        return post_processing.PostProcessingGaussianMixture(
+            scaler=scaler,
+            coord_convert=coord,
+            frame_to_emitter=frame2em,
+            ch_map=ch_map,
+        )
 
     def test_forward(self, post):
-        x = torch.rand(2, 10, 32, 32)
-        x[x > 0.01] = 0
+        x = torch.rand(2, 14, 32, 32)
+        if x.size(1) != post._ch_map.n:
+            raise ValueError("Wrong number of channels")
+
+        # set most of the probs to zero
+        x[:, post._ch_map.ix_prob][x[:, post._ch_map.ix_prob] < 0.98] = 0
+
+        # set x/y offset vectors to default offset range -0.5 to 0.5
+        x[:, post._ch_map.ix_xyz[:-1]] -= 0.5
 
         em = post.forward(x)
+
         assert isinstance(em, emitter.EmitterSet)
+        assert em.xyz[:, :2].min() >= -0.5
+        assert em.xyz[:, :2].max() < 31.5
+        assert (em.xyz[:, 2] > 100).any()
+        assert em.xyz[:, 2].max() < 800
 
 
 @pytest.mark.skip(reason="deprecated impl")
